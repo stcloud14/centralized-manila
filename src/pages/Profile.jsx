8dd5829e0ce7009c80eecc612f7871ff6370e1db
stcloud14 <adrianpaolomaghirang@gmail.com>
2023-10-16 22:30:16 +0800

    second commit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -77,7 +77,7 @@ function Profile() {
                         dateFormat: 'Y-m-d',
                         altInput: true,
                         altFormat: 'F j, Y', 
-                        altInputClass: 'block py-2.5 px-0 w-full text-sm text-gray-900 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer',
+                        altInputClass: 'block py-2.5 px-0 w-full bg-transparent text-sm text-gray-900 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer',
                         
                       }}
                       required
