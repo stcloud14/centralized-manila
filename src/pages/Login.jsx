818e29674060a92498a5c9b8009f9ec886120b81
stcloud14 <adrianpaolomaghirang@gmail.com>
2023-10-17 01:14:56 +0800

    eight commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
+import LoginForm from '../components/LoginForm';
 
 function Login() {
     return (
-        <div>
-            <h1>
-                hi
-            </h1>
-        </div>
+        <main>
+            <div className='grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1 ms-4 mt-4 me-4 gap-4'>
+            <div className="col-span-2">
+                <LoginForm />
+            </div>
+
+            <div className="col-span-1">
+                <Card02 />
+            </div>
+            
+        
+            </div>
+        </main>
     );
-}
+  }
+  
 
 export default Login
