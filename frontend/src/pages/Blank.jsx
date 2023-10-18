d49be592056b77258ee2a9be29d8c73b2d9a4dc5
stcloud14 <adrianpaolomaghirang@gmail.com>
2023-10-18 22:38:28 +0800

    fixingUI

--- frontend/src/pages/Blank.jsx
+++ frontend/src/pages/Blank.jsx
@@ -4,9 +4,6 @@ import Sidebar from '../partials/Sidebar';
 import Header from '../partials/Header';
 import Card01 from '../partials/news/Card01';
 import Card02 from '../partials/news/Card02';
-import Card03 from '../partials/news/Card03';
-import Card04 from '../partials/news/Card04';
-import Card05 from '../partials/news/Card05';
 
 function Blank() {
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -46,13 +46,13 @@ function Login() {
                   <div className="relative z-0 w-full group">
                     <input type="text" id="login_mobile" name="login_mobile" placeholder=' ' className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none text-black focus:outline-none focus:ring-0 focus:border-blue-600 peer" value={username}
                     onChange={(e) => setUsername(e.target.value)} />
-                    <label for="login_mobile" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Mobile Number</label>
+                    <label htmlFor="login_mobile" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Mobile Number</label>
                   </div>
 
                   <div className="relative z-0 w-full mb-6 group">
                     <input type="password" id="login_pass" name="login_pass" placeholder=' ' className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 appearance-none text-black focus:outline-none focus:ring-0 focus:border-blue-600 peer" value={password}
                     onChange={(e) => setPassword(e.target.value)} />
-                    <label for="login_pass" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>  
+                    <label htmlFor="login_pass" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>  
                   </div>
                 </div>
                 
