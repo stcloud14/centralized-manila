6710dc38678f1239203875201c55ff1226788b17
Frostoverthesheep <atanaciobasa9@gmail.com>
2023-10-24 01:22:49 +0800

    dev

--- frontend/src/pages/PersonalInfo.jsx
+++ frontend/src/pages/PersonalInfo.jsx
@@ -142,8 +142,8 @@ const PersonalInfo =()=>{
                   <input onChange={handleInputChange} value={userPersonal.suffix} type="text" name="suffix" id="suffix" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                   <label htmlFor="suffix" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Suffix</label>
                 </div>
-                <div className="relative z-0 w-full mb-6 group">
-                  <select value={userPersonal.sex_id} name="sex" id="sex" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" >
+                <div className="relative z-0 w-full mb-6 group" >
+                  <select onChange={handleInputChange} value={userPersonal.sex_id} name="sex_id" id="sex" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" >
                     <option value="0" disabled selected>Select Sex</option>
                     <option value="1" className='dark:bg-[#3d3d3d]'>Male</option>
                     <option value="2"className='dark:bg-[#3d3d3d]'>Female</option>
