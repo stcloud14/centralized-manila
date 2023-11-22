ae4b477b13fd95878ee08d40cc00b99269cab7d7
stcloud14 <adrianpaolomaghirang@gmail.com>
2023-11-22 15:27:41 +0800

    inayos tax UI

--- frontend/src/pages/RPTaxPayment.jsx
+++ frontend/src/pages/RPTaxPayment.jsx
@@ -113,7 +113,7 @@ const RPTaxPayment =()=>{
               </div>
               )}
   
-              <div className="grid md:grid-cols-2 md:gap-6">
+              <div className="grid md:grid-cols-1 md:gap-6 md:mx-96 mt-24">
                 <div className="relative z-0 w-full mb-6 group">
                   <input onChange={handleInputChange} value={userPersonal.rp_tdn}  type="text" name="rp_tdn" id="rp_tdn" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                   <label htmlFor="first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Tax Declaration Number (TDN)</label>
