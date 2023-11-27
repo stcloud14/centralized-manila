c9184821faf59a04597a505e7cd5a71fdf236d67
ruficarl <107566254+flaucirr@users.noreply.github.com>
2023-11-27 17:19:48 +0800

    rptax year

--- frontend/src/components/RPTaxPaymentForm.jsx
+++ frontend/src/components/RPTaxPaymentForm.jsx
@@ -86,6 +86,17 @@ const RPTaxPaymentForm =()=>{
   }));
 
 
+  } else if (name === 'rp_year') {
+    const { name, value } = e.target;
+
+  const formattedValue = value.replace(/\D/g, '');
+
+  setRptaxPayment((prevData) => ({
+    ...prevData,
+    [name]: formattedValue,
+  }));
+
+
   } else if (name === 'amount') {
     const { name, value } = e.target;
 
@@ -245,7 +256,7 @@ const handleCheckboxChange = (e) => {
                       
                       <div className="relative z-0 w-full mb-2 group">
                         <input
-                          type="text" name="rp_year" id="rp_year" placeholder=" " onChange={handleInputChange} value={rptaxPayment.rp_year}
+                          type="text" name="rp_year" id="rp_year" placeholder=" " onChange={handleInputChange} value={rptaxPayment.rp_year} maxLength={4}
                           className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                           required
                         />
