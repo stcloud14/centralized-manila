6a7635c05cfab30b4239d212d25577257f850131
Frostoverthesheep <atanaciobasa9@gmail.com>
2023-11-27 13:31:31 +0800

    format

--- frontend/src/components/RPTaxClearanceForm.jsx
+++ frontend/src/components/RPTaxClearanceForm.jsx
@@ -159,7 +159,7 @@ const RPTaxClearanceForm =()=>{
             <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-[#2b2b2b] dark:border-[#3d3d3d] shadow-lg rounded-sm border border-slate-200">
               <div className="px-5 py-5">
                 <form className="max-w-md mx-auto">
-                  <h1 className='font-medium text-center text-slate-700 dark:text-white'>Real Property Tax</h1>
+                  <h1 className='font-medium text-center text-slate-700 dark:text-white'>Tax Clearance</h1>
                   <h1 className='text-sm italic text-center text-slate-700 dark:text-gray-300 mb-6'>Tax Payment</h1>
 
                   {/* {isSuccess && (
