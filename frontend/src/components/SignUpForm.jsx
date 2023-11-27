82378a8b35dc2b150cfb6bdd7829260b775b9b7c
ruficarl <107566254+flaucirr@users.noreply.github.com>
2023-11-27 14:38:47 +0800

    formatted register mobile_no

--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -14,8 +14,36 @@ const SignUpForm =()=>{
 const navigate = useNavigate()
 
 const handleChange = (e) => {
-  const uppercasedValue = e.target.value.toUpperCase();
-  setUserReg((prev) => ({ ...prev, [e.target.name]: uppercasedValue }));
+  const { name, value } = e.target;
+
+  if (name === 'f_name' || name === 'l_name') {
+
+    const uppercasedValue = (name === 'f_name' || name === 'l_name') ? value.toUpperCase() : value;
+
+    setUserReg((prev) => ({ ...prev, [e.target.name]: uppercasedValue }));
+
+  } else if (name === 'mobile_no') {
+
+    const rawValue = value.replace('+63 - ', '');
+
+    const formattedValue = rawValue.replace(/\D/g, '');
+
+    let formattedWithDashes = '';
+    for (let i = 0; i < formattedValue.length; i++) {
+    if (i === 3 || i === 6) {
+      formattedWithDashes += ' - ';
+    }
+    formattedWithDashes += formattedValue[i];
+  }
+    
+    setUserReg((prev) => ({ ...prev, [name]: formattedWithDashes }));
+
+    
+
+    
+  } else {
+    setUserReg((prev) => ({ ...prev, [name]: [value] }));
+  }
 };
 
 
@@ -55,12 +83,12 @@ console.log(userReg)
                 </div>
 
                 <div className="relative z-0 w-full mb-6 group ">
-                    <input onChange={handleChange} type="text" name="mobile_no" id="mobile_no" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input onChange={handleChange} value={`+63 - ${userReg.mobile_no}`} maxLength={22} type="text" name="mobile_no" id="mobile_no" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label htmlFor="mobile_no" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Mobile Number (+63)</label>
                 </div>
 
                 <div className="relative z-0 w-full mb-6 group">
-                    <input onChange={handleChange} type="password" name="user_pass" id="user_pass" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input onChange={handleChange} value={userReg.user_pass} type="password" name="user_pass" id="user_pass" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label htmlFor="user_pass" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
                 </div>
 
@@ -70,6 +98,7 @@ console.log(userReg)
                 </div> */}
 
                 <div>
+                  <h1 className='italic text-xs'>Password must be:</h1>
                   <h1 className='italic text-xs'>- Minimum of 8 Characters</h1>
                   <h1 className='italic text-xs'>- At Least one uppercase and lowercase letter</h1>
                   <h1 className='italic text-xs'>- At least one symbol</h1>
