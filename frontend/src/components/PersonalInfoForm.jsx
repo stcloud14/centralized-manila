8ae607fef73db2373fa8eac4f4177fa39c0c22c3
stcloud14 <adrianpaolomaghirang@gmail.com>
2023-12-02 01:42:19 +0800

    nagtanggal ng space

--- frontend/src/components/PersonalInfoForm.jsx
+++ frontend/src/components/PersonalInfoForm.jsx
@@ -134,7 +134,6 @@ const PersonalInfoForm =()=>{
                 <div className="relative z-0 w-full mb-6 group">
                   <input onChange={handleChangePersonal} value={userPersonal.f_name} type="text" name="f_name" id="first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required/>
                   <label htmlFor="first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First Name</label>
-                
                 </div>
                 <div className="relative z-0 w-full mb-6 group">
                   <input onChange={handleChangePersonal} value={userPersonal.m_name} type="text" name="m_name" id="middle_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required/>
