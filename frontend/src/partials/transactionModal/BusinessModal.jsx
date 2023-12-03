2891543ae80747325d8c89c7bc4366063cbd29e0
Miaaaaah <jeremiahpaul612@gmail.com>
2023-12-04 00:28:03 +0800

    CLASSNAME

--- frontend/src/partials/transactionModal/BusinessModal.jsx
+++ frontend/src/partials/transactionModal/BusinessModal.jsx
@@ -67,7 +67,7 @@ const BusinessModal = ({ selectedTransaction }) => {
                           </div> */}
                           <div className="flex justify-between mb-1">
                             <span className="font-medium whitespace-nowrap">Status</span>
-                            <span class="px-2.5 py-1 text-xs font-semibold rounded-full bg-yellow-100 text-yellow-800">{selectedTransaction.status_type}</span>
+                            <span className="px-2.5 py-1 text-xs font-semibold rounded-full bg-yellow-100 text-yellow-800">{selectedTransaction.status_type}</span>
                           </div>
 
                           <hr className='mt-7 mb-1'/>
