cb09a8ac74fdf1319e21fb01af50fd457a866635
Miaaaaah <jeremiahpaul612@gmail.com>
2023-12-04 01:00:35 +0800

    HE

--- frontend/src/components/BirthCertificateForm.jsx
+++ frontend/src/components/BirthCertificateForm.jsx
@@ -131,7 +131,7 @@ const BirthCertificateForm =()=>{
                   </div>
                   <div className="relative z-0 w-full mb-6 group">
                     <select id="birthc_sex" defaultValue={0} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" >
-                      <option value="0" className='dark:bg-[#3d3d3d]' disabled>Select Sex</option>
+                      <option value="0" className='dark:bg-[#3d3d3d]'>Select Sex</option>
                       <option className='dark:bg-[#3d3d3d]'>Option</option>
                       <option className='dark:bg-[#3d3d3d]'>Option</option>
                     </select>
