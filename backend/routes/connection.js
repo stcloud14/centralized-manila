7a774fa082201eeb1cba4f43ccbc3c55d2d6ef42
ruficarl <107566254+flaucirr@users.noreply.github.com>
2023-12-04 16:01:02 +0800

    localhost to port no.

--- backend/routes/connection.js
+++ backend/routes/connection.js
@@ -1,7 +1,7 @@
 import mysql from "mysql";
 
 const conn2 = mysql.createConnection({
-    host: "localhost",
+    host: "127.0.0.1",
     user: "root",
     password: "admin",
     database: "clientdatabase",
