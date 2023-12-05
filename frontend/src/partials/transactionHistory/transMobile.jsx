b1f9de8338bf1a718c024ce99ba126c2ca0cc9b4
stcloud14 <adrianpaolomaghirang@gmail.com>
2023-12-05 21:21:12 +0800

    close mobile dropdown filter on transhistory when click outside

--- frontend/src/partials/transactionHistory/transMobile.jsx
+++ frontend/src/partials/transactionHistory/transMobile.jsx
@@ -36,11 +36,6 @@ const TransMobile = ({ searchInput, handleSearch, handleSearchInputChange, handl
 
                   <TransDropdownFilter handleSortChange={handleSortChange} sortOption={sortOption}/>
                   
-                  {/* <button className="bg-blue-500 hover:bg-blue-600 text-white px-1 py-1 rounded-md inline-flex items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                      <path className="stroke-white" strokeLinecap="round" strokeLinejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
-                    </svg>
-                  </button> */}
                 </div>
                 <div className="flex text-xs mb-5">
                   <button className="group flex justify-center w-full items-center text-center p-1 border border-emerald-500 text-emerald-500 hover:bg-emerald-500 hover:text-white rounded-full mt-2">
