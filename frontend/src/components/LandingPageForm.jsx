eb608f5b5fe273bd11e431417d01f4ec84a01a13
stcloud14 <adrianpaolomaghirang@gmail.com>
2023-12-10 23:32:55 +0800

    landing page light/dark mode

--- frontend/src/components/LandingPageForm.jsx
+++ frontend/src/components/LandingPageForm.jsx
@@ -54,7 +54,7 @@ const LandingPageForm =()=>{
   };
     
   return (
-    <div className="relative h-screen bg-[url('./src/images/manila-v2.jpg')] dark:bg-[url('./src/images/manila-hd.png')] lg:bg-cover bg-no-repeat bg-top">
+    <div className="relative h-screen bg-[url('./src/images/manila-hd.png')] dark:bg-[url('./src/images/manila-v2.jpg')] bg-cover bg-no-repeat bg-top">
       <div className="flex flex-col md:flex-row h-full">
         {/* Left Section */}
         <div className="md:w-1/2 flex items-center justify-center lg:mb-0 mb-5">
@@ -78,7 +78,7 @@ const LandingPageForm =()=>{
 
         {/* Right Section */}
         <div className="md:w-1/2 flex items-center justify-center lg:mx-auto mx-5">
-          <div className="bg-white dark:bg-opacity-100 bg-opacity-90 lg:p-16 p-10 rounded-3xl shadow-lg">
+          <div className="bg-white bg-opacity-90 lg:p-16 p-10 rounded-3xl shadow-lg">
           <h1 className='text-center mb-10 lg:text-lg text-md tracking-[.020em]'>
             <span className='font-medium text-slate-600'>"Maligayang pagdating, </span>
             <span className='font-bold text-blue-500'>Manileño!"</span>
@@ -130,7 +130,7 @@ const LandingPageForm =()=>{
         </div>
         <ThemeToggle/>
         {/* Footer */}
-        <div className="absolute bottom-0 w-full text-center text-sm text-gray-500 py-3 drop-shadow-xl">
+        <div className="md:absolute relative bottom-0 w-full text-center text-sm text-gray-500 py-3 drop-shadow-xl">
           Copyright &copy; {new Date().getFullYear()} |
           <a href="/indexadmin" className='hover:text-white'>
               <span className="hover:font-medium hover:text-blue-500"> B</span>
