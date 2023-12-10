cc368771c1f610623491c00542134cac66069bba
stcloud14 <adrianpaolomaghirang@gmail.com>
2023-12-10 23:40:15 +0800

    landing page light/dark mode

--- frontend/src/components/LandingPageForm.jsx
+++ frontend/src/components/LandingPageForm.jsx
@@ -78,7 +78,7 @@ const LandingPageForm =()=>{
 
         {/* Right Section */}
         <div className="md:w-1/2 flex items-center justify-center lg:mx-auto mx-5">
-          <div className="bg-white bg-opacity-90 lg:p-16 p-10 rounded-3xl shadow-lg">
+          <div className="bg-white bg-opacity-95 lg:p-16 p-10 rounded-3xl shadow-lg">
           <h1 className='text-center mb-10 lg:text-lg text-md tracking-[.020em]'>
             <span className='font-medium text-slate-600'>"Maligayang pagdating, </span>
             <span className='font-bold text-blue-500'>Manileño!"</span>
@@ -86,13 +86,13 @@ const LandingPageForm =()=>{
               <form onSubmit={handleSubmit}>
                     <div className="grid md:grid-cols-1 md:gap-6">
                       <div className="relative z-0 w-full lg:mb-0 mb-4 group">
-                        <input type="text" id="mobile_no" name="mobile_no" placeholder=' ' className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-400 appearance-none text-black focus:outline-none focus:ring-0 focus:border-blue-600 peer mobnum" 
+                        <input type="text" id="mobile_no" name="mobile_no" placeholder=' ' className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 dark:border-gray-400 appearance-none text-black focus:outline-none focus:ring-0 focus:border-blue-600 peer mobnum" 
                         value={`+63 ${userAuth.mobile_no}`} maxLength={14} onChange={handleChange}/>           
                         <label htmlFor="mobile_no" className="peer-focus:font-medium appearance-none absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Mobile Number (+63)</label>
                       </div>
 
                       <div className="relative z-0 w-full mb-6 group">
-                        <input type="password" id="user_pass" name="user_pass" placeholder=' ' className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-400 appearance-none text-black focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
+                        <input type="password" id="user_pass" name="user_pass" placeholder=' ' className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 dark:border-gray-400 appearance-none text-black focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
                         value={userAuth.user_pass} onChange={handleChange} />
                         <label htmlFor="user_pass" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>  
                       </div>
@@ -100,7 +100,7 @@ const LandingPageForm =()=>{
 
                     <div className="mt-4 flex justify-between font-semibold text-sm">
                         <label className="flex text-slate-500 hover:text-slate-600 cursor-pointer">
-                            <input className="mr-1.5 shrink-0 mt-0.5 border-2 border-gray-400 rounded bg-transparent text-emerald-600 pointer-events-none focus:ring-emerald-500" type="checkbox" />
+                            <input className="mr-1.5 shrink-0 mt-0.5 border-2 border-gray-300 dark:border-gray-400 rounded bg-transparent text-emerald-600 pointer-events-none focus:ring-emerald-500" type="checkbox" />
                             <span>Remember Me</span>
                         </label>
                         <a className="text-yellow-500 hover:text-blue-700 hover:text-yellow-600" href="#">Forgot Password?</a>
