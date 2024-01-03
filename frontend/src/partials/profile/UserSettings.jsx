ccfc8380c8b4f9db30cf3b4b01559adc2dac9949
stcloud14 <adrianpaolomaghirang@gmail.com>
2024-01-03 17:29:05 +0800

    usersettings.jsx

--- frontend/src/partials/profile/UserSettings.jsx
+++ frontend/src/partials/profile/UserSettings.jsx
@@ -56,16 +56,18 @@ const UserSettings =()=>{
                   </div>
 
                     {/* Submit Button */}
-                    <div className="flex justify-between items-end mt-10 mb-4">
-                      <button 
-                          type="submit" 
-                          className="text-red-500 hover:text-white border border-red-500 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-red-300 font-normal rounded-full text-sm px-10 py-2.5 text-center mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-500 dark:focus:ring-red-800">
-                            Delete Account
+                    <div className="flex flex-col sm:flex-row justify-center items-end mt-10 mb-4 space-x-10">
+                      <button
+                        type="submit"
+                        className="w-full sm:w-auto text-red-500 hover:text-white border border-red-500 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-red-300 font-normal rounded-full text-sm px-10 py-2.5 text-center mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-500 dark:focus:ring-red-800"
+                      >
+                        Delete Account
                       </button>
-                      <button 
-                          type="submit" 
-                          className="text-blue-500 hover:text-white border border-blue-500 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-full text-sm px-10 py-2.5 text-center mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
-                            Save Changes
+                      <button
+                        type="submit"
+                        className="w-full sm:w-auto text-blue-500 hover:text-white border border-blue-500 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-full text-sm px-10 py-2.5 text-center mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+                      >
+                        Save Changes
                       </button>
                     </div>
                 </form>
