271897a25cfe0bfe7c05c75e7b45410a64962c42
stcloud14 <adrianpaolomaghirang@gmail.com>
2024-01-03 17:45:08 +0800

    usersettings.jsx

--- frontend/src/partials/profile/UserSettings.jsx
+++ frontend/src/partials/profile/UserSettings.jsx
@@ -56,10 +56,10 @@ const UserSettings =()=>{
                   </div>
 
                     {/* Submit Button */}
-                    <div className="flex flex-col sm:flex-row justify-center items-end mt-10 mb-4 space-x-10">
+                    <div className="flex flex-col sm:flex-row justify-center items-end mt-10 mb-4 md:space-x-10">
                       <button
                         type="submit"
-                        className="w-full sm:w-auto text-red-500 hover:text-white border border-red-500 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-red-300 font-normal rounded-full text-sm px-10 py-2.5 text-center mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-500 dark:focus:ring-red-800"
+                        className="w-full sm:w-auto text-red-500 hover:text-white border border-red-500 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-red-300 font-normal rounded-full text-sm px-10 py-2.5 text-center md:mb-2 mb-4 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-500 dark:focus:ring-red-800"
                       >
                         Delete Account
                       </button>
