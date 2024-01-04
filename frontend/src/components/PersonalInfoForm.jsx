558a8d87d4e0c29311aff6545dee2d035bcdb9f7
stcloud14 <adrianpaolomaghirang@gmail.com>
2024-01-04 19:11:57 +0800

    birthdate UI during edit mode fixed

--- frontend/src/components/PersonalInfoForm.jsx
+++ frontend/src/components/PersonalInfoForm.jsx
@@ -310,9 +310,12 @@ const PersonalInfoForm =()=>{
                         placeholder: ' ', // Set an empty space as the initial placeholder
                       }}
                       className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                    
                     />
                   ) : (
-                    <div className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer">
+                    <div className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer ${
+                      !editMode && 'border-slate-200 dark:border-gray-700 cursor-not-allowed text-slate-500 dark:text-zinc-400'
+                    }`}>
                       {userBirth.birth_date}
                     </div>
                   )}
