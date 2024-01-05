21be4217101535308f3197dee415104da924cbac
stcloud14 <adrianpaolomaghirang@gmail.com>
2024-01-06 02:12:31 +0800

    footer pt.1

--- frontend/src/components/RPTaxClearanceForm.jsx
+++ frontend/src/components/RPTaxClearanceForm.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 import { useLocation } from 'react-router-dom';
 import Sidebar from '../partials/Sidebar';
 import Header from '../partials/Header';
+import Footer from '../partials/Footer';
 import Req from '../partials/misc/RequiredFieldIndicator';
 import TCTermsModal from '../partials/business/TCTermsModal';
 import ModalTransaction from '../partials/transactionModal/ModalTransaction';
@@ -176,6 +177,8 @@ const RPTaxClearanceForm =()=>{
     setIsModalVisible(!isModalVisible);
   };
 
+  const logoSrc = '../src/images/mnl_footer.svg';
+
   return (
     <div className="flex h-screen overflow-hidden dark:bg-[#212121]">
 
@@ -191,7 +194,7 @@ const RPTaxClearanceForm =()=>{
         <TermsModal isVisible={isModalVisible} onProceed={toggleModalVisibility} userID={user_id} />
 
         <main ref={contentRef} className="overflow-y-auto">
-          <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-[#2b2b2b] dark:border-[#3d3d3d] shadow-lg rounded-sm border border-slate-200 mx-4 my-4">
+          <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-[#2b2b2b] dark:border-[#3d3d3d] shadow-lg rounded-sm border border-slate-200 mx-4 mt-4 mb-2">
             <div className="px-5 py-5">
 
                 <form className={`max-w-md mx-auto ${isModalVisible ? 'blur' : ''}`}>
@@ -259,6 +262,7 @@ const RPTaxClearanceForm =()=>{
                 </form>
               </div>
             </div>
+            <Footer logo={logoSrc} />
         </main>
 
         {isModalOpen && (
---
+++ frontend/src/partials/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Footer = ({ logo }) => {
+  return (
+    <footer className="text-gray-500 text-xs mx-10">
+      <div className="flex flex-col items-center sm:flex-row sm:items-center mx-10">
+        <div className="mb-0">
+          <img src={logo} alt="Company Logo" className="h-20" />
+        </div>
+
+        <div className="flex flex-col items-center sm:flex-row sm:ml-auto">
+          <a href="#" className="sm:ml-0 sm:mr-8">News</a>
+          <a href="#" className="sm:ml-0 sm:mr-8">About</a>
+          <a href="#" className="sm:ml-0 sm:mr-8">Contact</a>
+          <a href="#" className="sm:ml-0 sm:mr-8">FAQs</a>
+          <a href="#" className="mb-2 sm:mb-0 sm:ml-0 sm:mr-8">Privacy Policy</a>
+        </div>
+      </div>
+
+      <div className="border-t border-gray-300 dark:border-gray-500 mx-10 my-4" />
+
+      <div className="text-center mt-2 mb-4 mx-10">
+        <span>© 2024 Centralized Manila. All rights reserved.</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
