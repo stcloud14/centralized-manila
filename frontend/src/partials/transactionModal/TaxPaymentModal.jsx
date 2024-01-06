b4c175ec97b4747cd3d440fd8167f569b481c1cd
Miaaaaah <jeremiahpaul612@gmail.com>
2024-01-06 21:54:47 +0800

    Button napupunta sa left

--- frontend/src/partials/transactionModal/TaxPaymentModal.jsx
+++ frontend/src/partials/transactionModal/TaxPaymentModal.jsx
@@ -203,39 +203,39 @@ const TaxPaymentModal = ({ selectedTransaction, onClose, onSubmit }) => {
                     </div>
                   </div>
                 </div>
-                <div className="bg-white dark:bg-[#212121] px-4 pt-3 pb-5 gap-3 sm:px-6 flex items-center justify-between">
+                <div className="bg-white dark:bg-[#212121] px-4 pt-3 pb-5 gap-3 sm:px-6 flex items-center">
                   {/* <img src="https://upload.wikimedia.org/wikipedia/commons/6/6c/Sample_EPC_QR_code.png" alt="QR Code" className="w-20 h-20 mr-3"/> */}
 
                   {status_type !== 'Paid' && (
-                  <button
-                    onClick={makePayment}
-                    type="button"
-                    className="text-slate-500 text-xs text-center px-5 py-2 mb-0 md:text-sm ms-2 hover:text-white border border-slate-500 hover:bg-slate-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-full dark:border-slate-500 dark:text-white dark:hover:text-white dark:hover:bg-slate-500 dark:focus:ring-slate-800"
-                  >
-                    <span className="font-semibold whitespace-nowrap ml-2"> PAY: {taxPaymentTransaction.amount ? taxPaymentTransaction.amount + '.00' : '-'}</span>
-                  </button>
-                )}
-                  
-                  <div className="flex items-center space-x-2 mt-auto">
-                      <button
-                          onClick={onClose}
-                          type="button"
-                          className="text-slate-500 text-xs text-center px-5 py-2 mb-0 md:text-sm ms-2 hover:text-white border border-slate-500 hover:bg-slate-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-full dark:border-slate-500 dark:text-white dark:hover:text-white dark:hover:bg-slate-500 dark:focus:ring-slate-800"
-                      >
-                          <p>Close</p>
-                      </button>
-
-                      {transaction_id ? null : (
-                      <button
-                          onClick={onSubmit}
-                          type="button"
-                          className="text-white text-xs md:text-sm bg-blue-500 border border-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-full px-5 py-2 text-center dark:border-blue-500 dark:text-white dark:hover:text-white dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                      >
-                          <p>Submit</p>
+                    <button
+                      onClick={makePayment}
+                      type="button"
+                      className="text-slate-500 text-xs text-center px-5 py-2 mb-0 md:text-sm ms-2 hover:text-white border border-slate-500 hover:bg-slate-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-full dark:border-slate-500 dark:text-white dark:hover:text-white dark:hover:bg-slate-500 dark:focus:ring-slate-800"
+                    >
+                      <span className="font-semibold whitespace-nowrap ml-2"> PAY: {taxPaymentTransaction.amount ? taxPaymentTransaction.amount + '.00' : '-'}</span>
                     </button>
+                  )}
+                  
+                  <div className="flex items-center space-x-2 ml-auto">
+                    <button
+                      onClick={onClose}
+                      type="button"
+                      className="text-slate-500 text-xs text-center px-5 py-2 mb-0 md:text-sm ms-2 hover:text-white border border-slate-500 hover:bg-slate-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-full dark:border-slate-500 dark:text-white dark:hover:text-white dark:hover:bg-slate-500 dark:focus:ring-slate-800"
+                    >
+                      <p>Close</p>
+                    </button>
+
+                    {transaction_id ? null : (
+                      <button
+                        onClick={onSubmit}
+                        type="button"
+                        className="text-white text-xs md:text-sm bg-blue-500 border border-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-full px-5 py-2 text-center dark:border-blue-500 dark:text-white dark:hover:text-white dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                      >
+                        <p>Submit</p>
+                      </button>
                     )}
                   </div>
-              </div>
+                </div>
 
               </div>
             </div>
