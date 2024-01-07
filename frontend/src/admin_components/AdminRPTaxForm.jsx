fdd4e2c61897c21f7271d6edbf896f5f8529633e
stcloud14 <adrianpaolomaghirang@gmail.com>
2024-01-07 21:08:55 +0800

    ADMIN rptax card shadow and reject modal

--- frontend/src/admin_components/AdminRPTaxForm.jsx
+++ frontend/src/admin_components/AdminRPTaxForm.jsx
@@ -4,6 +4,7 @@ import AdminHeader from '../admin_partials/AdminHeader';
 import AdminFooter from '../admin_partials/AdminFooter';
 import AdminRPTaxClearanceModal from '../admin_partials/admin_modals/AdminRPTaxClearanceModal';
 import AdminRPTaxPaymentModal from '../admin_partials/admin_modals/AdminRPTaxPaymentModal';
+import AdminRPTaxRejectModal from '../admin_partials/admin_modals/AdminRPTaxRejectModal';
 
 const AdminRPTaxForm =()=>{
 
@@ -32,6 +33,17 @@ const AdminRPTaxForm =()=>{
   const handleProcessSubmit2 = () => {
     setIsModalOpen2(false);
   };
+
+  const [isModalOpen3, setIsModalOpen3] = useState(false);
+  const handleOpenModal3 = () => {
+      setIsModalOpen3(true);
+    }
+  const handleCloseModal3 = () => {
+    setIsModalOpen3(false);
+  };
+  const handleReject = () => {
+    setIsModalOpen3(false);
+  };
   
 
   return (
@@ -48,13 +60,13 @@ const AdminRPTaxForm =()=>{
 
         {/*  Contents Area */}
         <main className="overflow-y-auto">
-          <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-[#2b2b2b] dark:border-[#3d3d3d] shadow-lg rounded-sm border border-slate-200 mx-4 my-4">
+          <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-[#2b2b2b] dark:border-[#3d3d3d] shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)] dark:shadow-[0_4px_6px_-1px_rgba(0,0,0,0.2)] rounded-sm border border-slate-200 mx-4 my-4">
             <div className="px-5 py-5">
               <h1 className='font-medium text-center text-slate-700 dark:text-white'>Real Property Tax</h1>
               <h1 className='mb-7 text-sm italic text-center text-slate-700 dark:text-gray-300'>Tax Payment and Tax Clearance Requests</h1> 
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 md:gap-6 gap-2">
                 {/* Tax Clearance Sample */}
-                <div className="bg-white dark:bg-[#333333] shadow-md rounded-sm mb-4 flex flex-col">
+                <div className="bg-white dark:bg-[#333333] shadow-[0_4px_10px_-1px_rgba(0,0,0,0.14)] dark:shadow-[0_4px_10px_-1px_rgba(0,0,0,0.2)] rounded-sm mb-4 flex flex-col">
                   <div className="text-xs font-semibold text-slate-60 bg-slate-200 dark:bg-[#212121] dark:text-white rounded-t-sm px-4 py-1.5">
                     Transaction ID:
                   </div>
@@ -72,7 +84,7 @@ const AdminRPTaxForm =()=>{
                   </div>
 
                   <div className="px-4 pb-5 space-x-4 flex justify-between items-center group">
-                    <div className="flex justify-center items-center text-center cursor-pointer p-1 border border-red-500 text-red-500 hover:bg-red-500 hover:text-white rounded-sm mt-2 flex-grow">
+                    <div onClick={handleOpenModal3} className="flex justify-center items-center text-center cursor-pointer p-1 border border-red-500 text-red-500 hover:bg-red-500 hover:text-white rounded-sm mt-2 flex-grow">
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
                       </svg>
@@ -88,7 +100,7 @@ const AdminRPTaxForm =()=>{
                 </div>
 
                 {/* Tax Payment Sample */}
-                <div className="bg-white dark:bg-[#333333] shadow-md rounded-sm mb-4 flex flex-col">
+                <div className="bg-white dark:bg-[#333333] shadow-[0_4px_10px_-1px_rgba(0,0,0,0.14)] dark:shadow-[0_4px_10px_-1px_rgba(0,0,0,0.2)] rounded-sm mb-4 flex flex-col">
                   <div className="text-xs font-semibold text-slate-60 bg-slate-200 dark:bg-[#212121] dark:text-white rounded-t-sm px-4 py-1.5">
                     Transaction ID:
                   </div>
@@ -138,6 +150,11 @@ const AdminRPTaxForm =()=>{
           isOpen2={isModalOpen2}
           handleClose2={handleCloseModal2}
           handleProcess2={handleProcessSubmit2}
+        />
+        <AdminRPTaxRejectModal
+          isOpen3={isModalOpen3}
+          handleClose3={handleCloseModal3}
+          handleReject={handleReject}
         />
       </div>
     </div>
