e8b232bdbc69ca1af97c8844b963dc1e22c4d49e
ruficarl <107566254+flaucirr@users.noreply.github.com>
2024-01-09 22:54:11 +0800

    admin dashboard change currency

--- frontend/src/charts/BarChart01.jsx
+++ frontend/src/charts/BarChart01.jsx
@@ -148,7 +148,7 @@ function BarChart01({
               label.style.fontSize = tailwindConfig().theme.fontSize.sm[0];
               label.style.lineHeight = tailwindConfig().theme.fontSize.sm[1].lineHeight;
               const theValue = c.data.datasets[item.datasetIndex].data.reduce((a, b) => a + b, 0);
-              const valueText = document.createTextNode(formatValue(theValue).replace(/^\$/, ''));
+              const valueText = document.createTextNode(theValue);
               const labelText = document.createTextNode(item.text);
               value.appendChild(valueText);
               label.appendChild(labelText);
