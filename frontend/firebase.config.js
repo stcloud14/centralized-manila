649332eb2ec6ff34e68fceb7a720ad711ca4cd06
Frostoverthesheep <atanaciobasa9@gmail.com>
2024-01-10 01:26:57 +0800

    m

---
+++ frontend/firebase.config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCAv_JVpRfCUBofOFS9MYJegjSa-A0YMfM",
+  authDomain: "centralized-manila.firebaseapp.com",
+  projectId: "centralized-manila",
+  storageBucket: "centralized-manila.appspot.com",
+  messagingSenderId: "568288119757",
+  appId: "1:568288119757:web:66a06534bfbd283184d284",
+  measurementId: "G-19ZTV6NFZV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export default auth;
--- frontend/src/components/OTPForm.jsx
+++ frontend/src/components/OTPForm.jsx
@@ -4,14 +4,16 @@ import { useNavigate } from 'react-router-dom';
 import ThemeToggle from '../partials/ThemeToggle';
 
 
+
 import { useLocation } from 'react-router-dom';
 
 const OTPForm =()=>{
-
-  
   const location = useLocation();
   const { pathname } = location;
   const user_id = pathname.split("/")[2];
+
+  const [otp, setOtp] = useState("");
+
 
   const [userAuth, setUserAuth] = useState({
       mobile_no:"",
@@ -28,7 +30,7 @@ const OTPForm =()=>{
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.get(`http://localhost:8800/login//${user_id}`);
+      const response = await axios.get(`http://localhost:8800/login/${user_id}`);
       if (response.status === 200) {
         // Authentication successful, navigate to the dashboard
         const {user_id} = response.data[0];
@@ -92,38 +94,41 @@ const OTPForm =()=>{
             <span className='font-medium text-slate-600'>Verification Code </span>
           </h1>
               <form onSubmit={handleSubmit}>
-                    <div className="grid md:grid-cols-1 md:gap-6">
-                      {/* <div className="relative z-0 w-full lg:mb-0 mb-4 group">
-                        <input type="text" id="mobile_no" name="mobile_no" placeholder=' ' className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 dark:border-gray-400 appearance-none text-black focus:outline-none focus:ring-0 focus:border-blue-600 peer mobnum" 
-                        value={`+63 ${userAuth.mobile_no}`} maxLength={14} onChange={handleChange}/>           
-                        <label htmlFor="mobile_no" className="peer-focus:font-medium appearance-none absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Mobile Number (+63)</label>
-                      </div> */}
-
-                      {/* <div className="relative z-0 w-full group">
-                        <input type="password" id="user_pass" name="user_pass" placeholder=' ' className="block py-2.5 px-0 w-full text-sm bg-transparent border-0 border-b-2 border-gray-300 dark:border-gray-400 appearance-none text-black focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
-                        value={userAuth.user_pass} onChange={handleChange} />
-                        <label htmlFor="user_pass" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>  
-                      </div> */}
+                  <div className="grid md:grid-cols-2 md:gap-6">
+                      <div className="relative z-0 w-full group">
+                      <input type="password" id="user_pass" name="user_pass" placeholder=' ' className="block py-2.5 px-0 w-full md:w-[147.5px] text-sm bg-transparent border-0 border-b-2 border-gray-300 dark:border-gray-400 appearance-none text-black focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
+                        value={userAuth.user_pass} />
+                      <label htmlFor="user_pass" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">NUMBER</label> 
+                      </div>
+                      <button
+                         className="text-blue-500 hover:text-white border border-blue-500 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-full text-sm px-10 py-2.5 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+                        type="submit"
+                      >
+                        verify OTP
+                      </button>
+                      
                     <div/>
                   </div>
 
                   <div className="grid md:grid-cols-2 md:gap-6">
+
                       <div className="relative z-0 w-full group">
                         <input type="password" id="user_pass" name="user_pass" placeholder=' ' className="block py-2.5 px-0 w-full md:w-[147.5px] text-sm bg-transparent border-0 border-b-2 border-gray-300 dark:border-gray-400 appearance-none text-black focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
-                        value={userAuth.user_pass} onChange={handleChange} />
+                        value={otp} onChange={handleChange} />
                         <label htmlFor="user_pass" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">OTP</label>  
                       </div>
-
+                      
                       <div className="text-center">
                       <button
                         className="text-blue-500 hover:text-white border border-blue-500 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-full text-sm px-10 py-2.5 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
                         type="submit"
                       >
-                        Verify OTP
+                        verify OTP
                       </button>
-                      {loginError && (
-                            <p className="text-red-600 p-2 text-xs rounded-full mt-5">{loginError}</p>
-                          )}
+
+                      
+
+
                     </div>
                     </div>
 
