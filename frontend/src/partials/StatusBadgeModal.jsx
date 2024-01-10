16c539d391ce83d48a7fb6ba6827f70042d65941
Miaaaaah <jeremiahpaul612@gmail.com>
2024-01-11 01:09:20 +0800

    StatusBadgeModal

--- frontend/src/partials/StatusBadgeModal.jsx
+++ frontend/src/partials/StatusBadgeModal.jsx
@@ -101,8 +101,8 @@ const StatusBadgeModal = ({ statusType }) => {
           type="button"
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
-          className="w-1 h-1">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 ml-1">
+          className="w-1 h-1 ml-1">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
             <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
           </svg>
         </button>
@@ -113,7 +113,7 @@ const StatusBadgeModal = ({ statusType }) => {
           style={getPopoverStyles()}
           className={`inline-block transition-opacity duration-300 bg-gray-50 dark:bg-[#333333] text-slate-700 dark:text-white border-gray-200 dark:border-gray-800 rounded-lg shadow-xl`}
         >
-          <div className="p-3 text-[10px] sm:text-xs w-[150px] sm:w-[300px] text-left">
+          <div className="p-3 text-[10px] sm:text-xs w-[150px] sm:w-[300px] text-left ">
             {text}
           </div>
         </div>
