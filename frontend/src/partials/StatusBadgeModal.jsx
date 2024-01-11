9692094775abbdac261953721702575b3d5babb5
Miaaaaah <jeremiahpaul612@gmail.com>
2024-01-11 16:46:37 +0800

    StatusBadgeModal

--- frontend/src/partials/StatusBadgeModal.jsx
+++ frontend/src/partials/StatusBadgeModal.jsx
@@ -94,19 +94,19 @@ const StatusBadgeModal = ({ statusType }) => {
 
   return (
     <div className="flex relative" ref={popoverRef} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      <span className={`text-[0.65rem] flex justify-center ml-1 py-0.5 rounded-full ${bgColor} ${textColor} w-24`}>
+      <span className={`text-[0.65rem] flex justify-center py-0.5 pl-1 rounded-full ${bgColor} ${textColor} w-24`}>
         <span className="font-semibold">{statusType}</span>
         <button
           onClick={togglePopover}
           type="button"
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
-          className="w-1 h-1 ml-1">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+          className="w-1 h-1">
+          
+        </button>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
             <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
           </svg>
-        </button>
-
         <div
           id="popover-description"
           role="tooltip"
