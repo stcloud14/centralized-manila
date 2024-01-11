34b92da4196ab4f6baaacd18352da67ff93056f9
Frostoverthesheep <atanaciobasa9@gmail.com>
2024-01-11 15:58:57 +0800

    otp

--- frontend/firebase.config.js
+++ frontend/firebase.config.js
@@ -8,12 +8,12 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCJ940c0oNSp6gdMLvowRcjJVgbytyCo8I",
-  authDomain: "centralized-manila-3a089.firebaseapp.com",
-  projectId: "centralized-manila-3a089",
-  storageBucket: "centralized-manila-3a089.appspot.com",
-  messagingSenderId: "1043594408268",
-  appId: "1:1043594408268:web:9151452b7f026c0e5b5fe9"
+  apiKey: "AIzaSyDK7GNS0d3_9olLywkTOVFeCxA3dGS1fXM",
+  authDomain: "centralized-manila-a410a.firebaseapp.com",
+  projectId: "centralized-manila-a410a",
+  storageBucket: "centralized-manila-a410a.appspot.com",
+  messagingSenderId: "100816911193",
+  appId: "1:100816911193:web:7799e6ef3fabf594cdea48"
 };
 
 // Initialize Firebase
