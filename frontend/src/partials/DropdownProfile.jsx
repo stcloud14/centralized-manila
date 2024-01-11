1504e3a37b5ab124350b48a88b623a3fac0db17a
Miaaaaah <jeremiahpaul612@gmail.com>
2024-01-11 17:34:51 +0800

    Verify Badge

--- frontend/src/partials/DropdownProfile.jsx
+++ frontend/src/partials/DropdownProfile.jsx
@@ -168,7 +168,7 @@ const DropdownProfile = ({ align }) => {
           src={userImage ? userImage : defaultImg}
           onError={(e) => console.error('Error loading image:', e)}
         />
-        <svg xmlns="http://www.w3.org/2000/svg" className="w-[30px] h-[30px] pb-3 text-blue-400 absolute bottom-0 right-[3.8rem] z-10 transform translate-x-1/2 translate-y-1/2" viewBox="0 0 841.89 595.28">
+        <svg xmlns="http://www.w3.org/2000/svg" className="w-[30px] h-[30px] pb-3 text-blue-400 absolute bottom-[-0rem] right-[4.08rem] z-10 transform translate-x-1/2 translate-y-1/2" viewBox="0 0 841.89 595.28">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -6 23 38" fill="white">
                       <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
                     </svg>
