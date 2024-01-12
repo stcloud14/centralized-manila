e6de5e9fa7ac8cfcf2b36ee9509fae7fb37602b4
stcloud14 <adrianpaolomaghirang@gmail.com>
2024-01-12 22:26:49 +0800

    ADMIN user view modal progress

--- frontend/src/admin_partials/admin_modals/AdminUserViewModal.jsx
+++ frontend/src/admin_partials/admin_modals/AdminUserViewModal.jsx
@@ -4,15 +4,15 @@ import defaultImage from '../../images/default_img.png';
 const AdminUserViewModal = ({ isOpen, handleClose }) => {
   return (
     isOpen && (
-      <div className="fixed z-50 inset-0">
-        <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center text-xs md:text-sm sm:block sm:p-0">
+      <div className="fixed z-50 inset-0 ">
+        <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center text-xs md:md:text-sm text-xs sm:block sm:p-0">
           <div className="fixed inset-0 transition-opacity" aria-hidden="true">
             <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
           </div>
           <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
             &#8203;
           </span>
-          <div className="inline-block align-bottom bg-white dark:bg-[#333333] rounded-sm text-center overflow-y-auto overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:w-full max-w-2xl max-h-screen relative">
+          <div className="inline-block align-bottom bg-white dark:bg-[#333333] rounded-sm text-center overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:w-full max-w-2xl max-h-screen relative">
             {/* Menu Bar */}
             <div className="bg-slate-200 dark:bg-[#212121] pt-1.5 pb-1 items-center">
               <button
@@ -28,8 +28,8 @@ const AdminUserViewModal = ({ isOpen, handleClose }) => {
             </div>
 
             {/* Content */}
-            <div className="px-4 pt-5 pb-3 sm:p-6 sm:pb-6">
-              <div className="mx-auto">
+            <div className="px-4 pt-5 pb-3 sm:p-6 sm:pb-6 overflow-y-auto max-h-[38rem]">
+            <div className="mx-auto">
                 <div className="mb-5">
                   <img
                     name="userImage"
@@ -41,32 +41,44 @@ const AdminUserViewModal = ({ isOpen, handleClose }) => {
                   <span className='font-bold text-lg text-gray-700 dark:text-white'>Personal Information</span>
                   <form>
                     <div className="mt-5 md:px-32 px-12">
-                      <label className="block font-medium text-sm text-gray-700 dark:text-white text-left py-1 px-0.5">First Name</label>
-                      <input type="text" className="block w-full text-sm rounded-md border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6" />
+                      <label className="block font-medium md:text-sm text-xs text-gray-700 dark:text-white text-left py-1 px-0.5">First Name</label>
+                      <input type="text" className="block w-full md:text-sm text-xs rounded border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:md:text-sm text-xs sm:leading-6" />
                     </div>
                     <div className="mt-2 md:px-32 px-12">
-                      <label className="block font-medium text-sm text-gray-700 dark:text-white text-left py-1 px-0.5">Middle Name</label>
-                      <input type="text" className="block w-full text-sm rounded-md border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6" />
+                      <label className="block font-medium md:text-sm text-xs text-gray-700 dark:text-white text-left py-1 px-0.5">Middle Name</label>
+                      <input type="text" className="block w-full md:text-sm text-xs rounded border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:md:text-sm text-xs sm:leading-6" />
                     </div>
                     <div className="mt-2 md:px-32 px-12">
-                      <label className="block font-medium text-sm text-gray-700 dark:text-white text-left py-1 px-0.5">Last Name</label>
-                      <input type="text" className="block w-full text-sm rounded-md border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6" />
+                      <label className="block font-medium md:text-sm text-xs text-gray-700 dark:text-white text-left py-1 px-0.5">Last Name</label>
+                      <input type="text" className="block w-full md:text-sm text-xs rounded border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:md:text-sm text-xs sm:leading-6" />
                     </div>
                     <div className="mt-2 md:px-32 px-12">
-                      <label className="block font-medium text-sm text-gray-700 dark:text-white text-left py-1 px-0.5">Suffix</label>
-                      <input type="text" className="block w-full text-sm rounded-md border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6" />
+                      <label className="block font-medium md:text-sm text-xs text-gray-700 dark:text-white text-left py-1 px-0.5">Suffix</label>
+                      <input type="text" className="block w-full md:text-sm text-xs rounded border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:md:text-sm text-xs sm:leading-6" />
                     </div>
                     <div className="mt-2 md:px-32 px-12">
-                      <label className="block font-medium text-sm text-gray-700 dark:text-white text-left py-1 px-0.5">Sex</label>
-                      <input type="text" className="block w-full text-sm rounded-md border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6" />
+                      <label className="block font-medium md:text-sm text-xs text-gray-700 dark:text-white text-left py-1 px-0.5">Sex</label>
+                      <input type="text" className="block w-full md:text-sm text-xs rounded border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:md:text-sm text-xs sm:leading-6" />
                     </div>
                     <div className="mt-2 md:px-32 px-12">
-                      <label className="block font-medium text-sm text-gray-700 dark:text-white text-left py-1 px-0.5">Date of Birth</label>
-                      <input type="text" className="block w-full text-sm rounded-md border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6" />
+                      <label className="block font-medium md:text-sm text-xs text-gray-700 dark:text-white text-left py-1 px-0.5">Date of Birth</label>
+                      <input type="text" className="block w-full md:text-sm text-xs rounded border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:md:text-sm text-xs sm:leading-6" />
                     </div>
                     <div className="mt-2 md:px-32 px-12">
-                      <label className="block font-medium text-sm text-gray-700 dark:text-white text-left py-1 px-0.5">Place of Birth</label>
-                      <input type="text" className="block w-full text-sm rounded-md border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6" />
+                      <label className="block font-medium md:text-sm text-xs text-gray-700 dark:text-white text-left py-1 px-0.5">Place of Birth</label>
+                      <input type="text" className="block w-full md:text-sm text-xs rounded border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:md:text-sm text-xs sm:leading-6" />
+                    </div>
+                    <div className="mt-2 md:px-32 px-12">
+                      <label className="block font-medium md:text-sm text-xs text-gray-700 dark:text-white text-left py-1 px-0.5">Civil Status</label>
+                      <input type="text" className="block w-full md:text-sm text-xs rounded border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:md:text-sm text-xs sm:leading-6" />
+                    </div>
+                    <div className="mt-2 md:px-32 px-12">
+                      <label className="block font-medium md:text-sm text-xs text-gray-700 dark:text-white text-left py-1 px-0.5">Citizenship</label>
+                      <input type="text" className="block w-full md:text-sm text-xs rounded border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:md:text-sm text-xs sm:leading-6" />
+                    </div>
+                    <div className="mt-2 md:px-32 px-12">
+                      <label className="block font-medium md:text-sm text-xs text-gray-700 dark:text-white text-left py-1 px-0.5">Residency Status</label>
+                      <input type="text" className="block w-full md:text-sm text-xs rounded border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:md:text-sm text-xs sm:leading-6" />
                     </div>
                   </form>
                 </div>
