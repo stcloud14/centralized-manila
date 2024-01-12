8eea3b0a49abbf52af1647123933489d54ae5b80
stcloud14 <adrianpaolomaghirang@gmail.com>
2024-01-13 01:37:55 +0800

    ADMIN User Registry Page Done

--- frontend/src/admin_partials/admin_modals/AdminUserViewModal.jsx
+++ frontend/src/admin_partials/admin_modals/AdminUserViewModal.jsx
@@ -1,13 +1,39 @@
-import React, { useState } from 'react';
-import defaultImage from '../../images/default_img.png';
-import Flatpickr from 'react-flatpickr';
+import React, { useState, useEffect } from 'react';
 import 'flatpickr/dist/themes/airbnb.css';
+import defaultImage from '../../images/default_img.png';
 import PersonalInfo from '../admin_userregistry/PersonalInfo';
 import ContactInfo from '../admin_userregistry/ContactInfo';
 import GovInfo from '../admin_userregistry/GovInfo';
+import AdminUserDeleteModal from '../admin_modals/AdminUserDeleteModal';
 
-const AdminUserViewModal = ({ isOpen, handleClose }) => {
-  const [userBirth, setUserBirth]=useState({})
+const AdminUserViewModal = ({ isOpen, handleClose, isOpen3, handleClose3 }) => {
+  const [userBirth, setUserBirth] = useState({});
+  const [editMode, setEditMode] = useState(false);
+
+  const handleEditClick = () => {
+    setEditMode(!editMode);
+  };
+
+  const handleSaveChanges = () => {
+    // Add code to save changes
+    setEditMode(false);
+  };
+
+  useEffect(() => {
+    if (!isOpen) {
+      // Reset editMode when the modal is closed
+      setEditMode(false);
+    }
+  }, [isOpen]);
+
+
+  const [isModalOpen3, setIsModalOpen3] = useState(false);
+  const handleOpenModal3 = () => {
+      setIsModalOpen3(true);
+    }
+  const handleCloseModal3 = () => {
+    setIsModalOpen3(false);
+  };
 
   return (
     isOpen && (
@@ -22,10 +48,7 @@ const AdminUserViewModal = ({ isOpen, handleClose }) => {
           <div className="inline-block align-bottom bg-white dark:bg-[#333333] rounded-sm text-center overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:w-full max-w-2xl max-h-screen relative">
             {/* Menu Bar */}
             <div className="bg-slate-200 dark:bg-[#212121] pt-1.5 pb-1 items-center">
-              <button
-                onClick={handleClose}
-                type="button"
-                className="float-right text-slate-500 text-xs md:text-sm"
+              <button onClick={handleClose} type="button" className="float-right text-slate-500 text-xs md:text-sm"
               >
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="md:w-5 md:h-5 w-4 h-4 mr-1">
                   <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
@@ -36,7 +59,47 @@ const AdminUserViewModal = ({ isOpen, handleClose }) => {
 
             {/* Content */}
             <div className="px-4 pt-5 pb-3 sm:p-6 sm:pb-6 overflow-y-auto max-h-[38rem]">
-            <div className="mx-auto">
+              <div className="mx-auto">
+                <div className="flex items-center justify-between mb-5">
+                  <div className="flex items-center text-xs">
+                    <button
+                      className={`text-white font-medium ${
+                        editMode ? 'bg-slate-400 dark:bg-slate-500' : 'dark:bg-blue-500'
+                      } ${
+                        editMode ? 'hover:bg-slate-500 dark:hover:bg-slate-600' : 'hover:bg-blue-600 dark:hover:bg-blue-600'
+                      } flex items-center bg-blue-500 hover:bg-blue-600 hover:border-blue-500 rounded-sm px-2 py-1.5`}
+                      onClick={handleEditClick}
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mr-1">
+                        {editMode ? <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" /> : <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />}
+                      </svg>
+                      <span>{editMode ? 'Cancel' : 'Edit User'}</span>
+                    </button>
+                    {editMode && (
+                      <button
+                        className="text-white font-medium dark:text-white dark:bg-emerald-500 dark:hover:bg-emerald-600 flex items-center bg-emerald-500 hover:bg-emerald-600 hover:border-emerald-500 rounded-sm px-2 py-1.5 ml-2"
+                        onClick={handleSaveChanges}
+                      >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mr-1">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                        </svg>
+                        <span>Save</span>
+                      </button>
+                    )}
+                  </div>
+                  <div className="flex items-center text-xs">
+                    <button
+                      className="text-white font-medium dark:text-white dark:bg-red-500 dark:hover:bg-red-600 flex items-center bg-red-500 hover:bg-red-600 hover:border-red-600 rounded-sm px-2 py-1.5"
+                      onClick={handleOpenModal3}
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mr-1">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                      </svg>
+                      <span>Delete User</span>
+                    </button>
+                  </div>
+                </div>
+
                 <div className="mb-5">
                   <img
                     name="userImage"
@@ -51,7 +114,12 @@ const AdminUserViewModal = ({ isOpen, handleClose }) => {
             </div>
           </div>
         </div>
+        <AdminUserDeleteModal
+          isOpen3={isModalOpen3}
+          handleClose3={handleCloseModal3}
+        />
       </div>
+      
     )
   );
 };
--- frontend/src/admin_partials/admin_userregistry/UserListDesktop.jsx
+++ frontend/src/admin_partials/admin_userregistry/UserListDesktop.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const UserListDesktop = ({handleOpenModal, handleOpenModal3}) => {
+const UserListDesktop = ({handleOpenModal}) => {
 
     return (
         <>
@@ -89,18 +89,12 @@ const UserListDesktop = ({handleOpenModal, handleOpenModal3}) => {
                   <td className="px-1 py-2 whitespace-nowrap text-xs md:text-sm text-slate-500 dark:text-slate-400">
                     Unverified
                   </td>
-                  <td className="py-1 whitespace-nowrap w-36">
+                  <td className="py-1 whitespace-nowrap">
                     <div className="flex justify-center gap-4 px-2">
                       <div onClick={handleOpenModal} className="group cursor-pointer flex items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-blue-500 hover:text-blue-600">
                           <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                           <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                        </svg>
-                      </div>
-
-                      <div onClick={handleOpenModal3} className="group cursor-pointer flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-red-500 hover:text-red-600">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                         </svg>
                       </div>
                     </div>
