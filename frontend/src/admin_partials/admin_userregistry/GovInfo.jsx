a696e874e49884776c92b46c1470dd5dab5aad2c
stcloud14 <adrianpaolomaghirang@gmail.com>
2024-01-13 01:45:07 +0800

    pota di pantay yung isang field mali yung margin top

--- frontend/src/admin_partials/admin_userregistry/GovInfo.jsx
+++ frontend/src/admin_partials/admin_userregistry/GovInfo.jsx
@@ -17,7 +17,7 @@ const GovInfo = () => {
                       <label htmlFor="user_philh_id" className="block font-medium md:text-sm text-xs text-gray-700 dark:text-white text-left py-1 px-0.5">Philhealth Number</label>
                       <input type="text" name="user_philh_id" id="user_philh_id" className="block w-full md:text-sm text-xs rounded border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:md:text-sm text-xs sm:leading-6" />
                     </div>
-                    <div className="mt-5 md:px-32 px-12">
+                    <div className="mt-2 md:px-32 px-12">
                       <label htmlFor="user_sss_id" className="block font-medium md:text-sm text-xs text-gray-700 dark:text-white text-left py-1 px-0.5">SSS Number</label>
                       <input type="text" name="user_sss_id" id="user_sss_id" className="block w-full md:text-sm text-xs rounded border-0 py-1.5 text-gray-900 dark:text-white dark:bg-[#3d3d3d] shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:md:text-sm text-xs sm:leading-6" />
                     </div>
