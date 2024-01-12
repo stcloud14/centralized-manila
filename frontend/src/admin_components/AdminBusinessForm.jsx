f0c18f83dfda61d30af3165c9f469dea6bdfc6d1
stcloud14 <adrianpaolomaghirang@gmail.com>
2024-01-12 18:53:49 +0800

    ADMIN Business Page

--- frontend/src/admin_components/AdminBusinessForm.jsx
+++ frontend/src/admin_components/AdminBusinessForm.jsx
@@ -1,26 +1,37 @@
 import React, { useState } from 'react';
+
 import { useLocation } from 'react-router-dom'; // Import useLocation from react-router-dom
 import AdminSidebar from '../admin_partials/AdminSidebar';
 import AdminHeader from '../admin_partials/AdminHeader';
 import AdminFooter from '../admin_partials/AdminFooter';
-import AdminRPTaxClearanceModal from '../admin_partials/admin_modals/AdminRPTaxClearanceModal';
-import AdminRPTaxRejectModal from '../admin_partials/admin_modals/AdminRPTaxRejectModal';
+
+import AdminRPView from '../admin_partials/admin_modals/AdminRPView';
+import AdminRPExpired from '../admin_partials/admin_modals/AdminRPExpired';
+import AdminRPProcess from '../admin_partials/admin_modals/AdminRPProcess';
+import AdminRPReject from '../admin_partials/admin_modals/AdminRPReject';
+import AdminRPDone from '../admin_partials/admin_modals/AdminRPDone';
+
+import AdminBusinessRequests from '../admin_partials/admin_cards/AdminBusinessRequests';
+import AdminBusinessProcessing from '../admin_partials/admin_cards/AdminBusinessProcessing';
+
+
 
 const AdminBusinessForm =()=>{
 
+
+  
   const location = useLocation();
   const { pathname, state } = location;
   console.log("pathname", pathname);
   const admin_type = pathname.split("/")[2];
 
-
   console.log("userrole", admin_type)
-
 
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   const logoSrc = '../src/images/mnl_footer.svg';
 
+  // View Details Modal
   const [isModalOpen, setIsModalOpen] = useState(false);
   const handleOpenModal = () => {
       setIsModalOpen(true);
@@ -28,31 +39,51 @@ const AdminBusinessForm =()=>{
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
-  const handleProcessSubmit = () => {
-    setIsModalOpen(false);
+
+   // Expired Modal
+   const [isModalOpen2, setIsModalOpen2] = useState(false);
+   const handleOpenModal2 = () => {
+       setIsModalOpen2(true);
+     }
+   const handleCloseModal2 = () => {
+     setIsModalOpen2(false);
+   };
+
+   // Process Modal
+   const [isModalOpen3, setIsModalOpen3] = useState(false);
+   const handleOpenModal3 = () => {
+       setIsModalOpen3(true);
+     }
+   const handleCloseModal3 = () => {
+     setIsModalOpen3(false);
+   };
+
+   // Reject Modal
+   const [isModalOpen4, setIsModalOpen4] = useState(false);
+   const handleOpenModal4 = () => {
+       setIsModalOpen4(true);
+     }
+   const handleCloseModal4 = () => {
+     setIsModalOpen4(false);
+   };
+
+   // Done Modal
+   const [isModalOpen5, setIsModalOpen5] = useState(false);
+   const handleOpenModal5 = () => {
+       setIsModalOpen5(true);
+     }
+   const handleCloseModal5 = () => {
+     setIsModalOpen5(false);
+   }
+
+
+
+
+  const handleProcessModal = (event) => {
+    event.stopPropagation();
+    console.log('Processing')
   };
 
-  const [isModalOpen2, setIsModalOpen2] = useState(false);
-  const handleOpenModal2 = () => {
-      setIsModalOpen2(true);
-    }
-  const handleCloseModal2 = () => {
-    setIsModalOpen2(false);
-  };
-  const handleProcessSubmit2 = () => {
-    setIsModalOpen2(false);
-  };
-
-  const [isModalOpen3, setIsModalOpen3] = useState(false);
-  const handleOpenModal3 = () => {
-      setIsModalOpen3(true);
-    }
-  const handleCloseModal3 = () => {
-    setIsModalOpen3(false);
-  };
-  const handleReject = () => {
-    setIsModalOpen3(false);
-  };
   
 
   return (
@@ -69,96 +100,59 @@ const AdminBusinessForm =()=>{
 
         {/*  Contents Area */}
         <main className="overflow-y-auto">
-          <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-[#2b2b2b] dark:border-[#3d3d3d] shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)] dark:shadow-[0_4px_6px_-1px_rgba(0,0,0,0.2)] rounded-sm border border-slate-200 mx-4 my-4">
+          {/*  Banner */}
+          <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-[#2b2b2b] dark:border-[#3d3d3d] shadow-sm rounded-sm border border-slate-200 mx-4 my-4">
             <div className="px-5 py-5">
-              <h1 className='font-medium text-center text-slate-700 dark:text-white'>Business Permit</h1>
-              <h1 className='mb-7 text-sm italic text-center text-slate-700 dark:text-gray-300'>Business Permit Requests</h1> 
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 md:gap-6 gap-2">
-                {/* Tax Clearance Sample */}
-                <div className="bg-white dark:bg-[#333333] shadow-[0_4px_10px_-1px_rgba(0,0,0,0.14)] dark:shadow-[0_4px_10px_-1px_rgba(0,0,0,0.2)] rounded-sm mb-4 flex flex-col">
-                  <div className="text-xs font-semibold text-slate-60 bg-slate-200 dark:bg-[#212121] dark:text-white rounded-t-sm px-4 py-1.5">
-                    Transaction ID:
-                  </div>
+              <h1 className="font-medium text-center text-slate-700 dark:text-white">Business Permit</h1>
+              <h1 className="mb-7 text-sm italic text-center text-slate-700 dark:text-gray-300">Transactions</h1>
 
-                  <div className="flex-grow px-4 pt-5 pb-4">
-                    <div className="text-xs text-slate-600 dark:text-slate-300 my-1">Type: Tax Clearance</div>
-                    <div className="text-xs text-slate-600 dark:text-slate-300 my-1">TDN: </div>
-                    <div className="text-xs text-slate-600 dark:text-slate-300 my-1">PIN:   </div>
-                    <div className="text-xs text-slate-600 dark:text-slate-300 my-1">Date Processed:   </div>
-                    <div className="text-xs text-slate-600 dark:text-slate-300 my-1">Time Processed:   </div>
-                    <div className="flex justify-start items-center text-xs text-slate-600 dark:text-slate-300 my-1">
-                      <span>Status:</span>
-                    </div>
-                    <div className="text-xs text-slate-600 dark:text-slate-300 my-1">Amount Paid: P</div>
-                  </div>
-
-                  <div className="px-4 pb-5 space-x-4 flex justify-between items-center group">
-                    <div onClick={handleOpenModal3} className="flex justify-center items-center text-center cursor-pointer p-1 border border-red-500 text-red-500 hover:bg-red-500 hover:text-white rounded-sm mt-2 flex-grow">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                      </svg>
-                      <span className="text-xs font-normal">&nbsp;Reject</span>
-                    </div>
-                    <div onClick={handleOpenModal} className="flex justify-center items-center text-center cursor-pointer p-1 border border-emerald-500 text-emerald-500 hover:bg-emerald-500 hover:text-white rounded-sm mt-2 flex-grow">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-                      </svg>
-                      <span className="text-xs font-normal">&nbsp;Process</span>
-                    </div>
-                  </div>
+              <div className="flex items-center justify-center space-x-6 text-xs">
+                <div className="flex items-center">
+                  <div className="w-4 h-1 mr-2 bg-[#d62d20]"></div>
+                  <p className="text-slate-700 dark:text-white">Business Permit</p>
                 </div>
-
-                {/* Tax Payment Sample */}
-                <div className="bg-white dark:bg-[#333333] shadow-[0_4px_10px_-1px_rgba(0,0,0,0.14)] dark:shadow-[0_4px_10px_-1px_rgba(0,0,0,0.2)] rounded-sm mb-4 flex flex-col">
-                  <div className="text-xs font-semibold text-slate-60 bg-slate-200 dark:bg-[#212121] dark:text-white rounded-t-sm px-4 py-1.5">
-                    Transaction ID:
-                  </div>
-
-                  <div className="flex-grow px-4 pt-5 pb-4">
-                    <div className="text-xs text-slate-600 dark:text-slate-300 my-1">Type: Tax Payment</div>
-                    <div className="text-xs text-slate-600 dark:text-slate-300 my-1">Account Name:   </div>
-                    <div className="text-xs text-slate-600 dark:text-slate-300 my-1">TDN: </div>
-                    <div className="text-xs text-slate-600 dark:text-slate-300 my-1">PIN:   </div>
-                    <div className="text-xs text-slate-600 dark:text-slate-300 my-1">From:   </div>
-                    <div className="text-xs text-slate-600 dark:text-slate-300 my-1">To:   </div>
-                    <div className="text-xs text-slate-600 dark:text-slate-300 my-1">Date Processed:   </div>
-                    <div className="text-xs text-slate-600 dark:text-slate-300 my-1">Time Processed:   </div>
-                    <div className="flex justify-start items-center text-xs text-slate-600 dark:text-slate-300 my-1">
-                      <span>Status:</span>
-                    </div>
-                    <div className="text-xs text-slate-600 dark:text-slate-300 my-1">Amount Paid: P</div>
-                  </div>
-
-                  <div className="px-4 pb-5 space-x-4 flex justify-between items-center group">
-                    <div className="flex justify-center items-center text-center cursor-pointer p-1 border border-red-500 text-red-500 hover:bg-red-500 hover:text-white rounded-sm mt-2 flex-grow">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                      </svg>
-                      <span className="text-xs font-normal">&nbsp;Reject</span>
-                    </div>
-                    <div onClick={handleOpenModal2} className="flex justify-center items-center text-center cursor-pointer p-1 border border-emerald-500 text-emerald-500 hover:bg-emerald-500 hover:text-white rounded-sm mt-2 flex-grow">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-                      </svg>
-                      <span className="text-xs font-normal">&nbsp;Process</span>
-                    </div>
-                  </div>
-                </div>
-
               </div>
             </div>
           </div>
+
+          {/*  Two Sections */}
+          <div className="grid grid-cols-12 gap-4 mx-4 my-4">
+            
+            <AdminBusinessRequests
+            handleOpenModal={handleOpenModal}
+            handleOpenModal2={handleOpenModal2}
+            handleOpenModal3={handleOpenModal3}
+            />
+            <AdminBusinessProcessing
+            handleOpenModal={handleOpenModal}
+            handleOpenModal4={handleOpenModal4}
+            handleOpenModal5={handleOpenModal5}
+            />
+
+          </div>
+
           <AdminFooter logo={logoSrc} />
         </main>
-        <AdminRPTaxClearanceModal
+
+        <AdminRPView
           isOpen={isModalOpen}
           handleClose={handleCloseModal}
-          handleProcess={handleProcessSubmit}
         />
-        <AdminRPTaxRejectModal
+        <AdminRPExpired
+          isOpen2={isModalOpen2}
+          handleClose2={handleCloseModal2}
+        />
+        <AdminRPProcess
           isOpen3={isModalOpen3}
           handleClose3={handleCloseModal3}
-          handleReject={handleReject}
+        />
+        <AdminRPReject
+          isOpen4={isModalOpen4}
+          handleClose4={handleCloseModal4}
+        />
+        <AdminRPDone
+          isOpen5={isModalOpen5}
+          handleClose5={handleCloseModal5}
         />
       </div>
     </div>
