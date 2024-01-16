0bfc9e0d0c5041b436294ebefe0f3f193a7130b8
ruficarl <107566254+flaucirr@users.noreply.github.com>
2024-01-16 19:46:47 +0800

    hidden username header

--- frontend/src/partials/DropdownProfile.jsx
+++ frontend/src/partials/DropdownProfile.jsx
@@ -200,7 +200,7 @@ const DropdownProfile = ({ align }) => {
 
 
         <div className="flex items-center truncate">
-          <span className="truncate ml-2 mr-1 text-sm font-medium dark:text-slate-300 group-hover:text-slate-800 dark:group-hover:text-slate-200">{userPersonal.f_name}</span>
+          {/* <span className="truncate ml-2 mr-1 text-sm font-medium dark:text-slate-300 group-hover:text-slate-800 dark:group-hover:text-slate-200">{userPersonal.f_name}</span> */}
           <svg className="w-3 h-3 shrink-0 ml-1 fill-current text-slate-400" viewBox="0 0 12 12">
             <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
           </svg>
