92c9ad104b26fc68756556c158328db29a7555c5
stcloud14 <adrianpaolomaghirang@gmail.com>
2024-01-16 22:44:57 +0800

    Landing Page UI Password Show/Hide Icon

--- frontend/src/components/LandingPageForm.jsx
+++ frontend/src/components/LandingPageForm.jsx
@@ -182,6 +182,11 @@ const LandingPageForm = () => {
         }
       };
       
+
+      const [showPassword, setShowPassword] = useState(false);
+      const handleTogglePassword = () => {
+        setShowPassword((prevShowPassword) => !prevShowPassword);
+      };
     
   return (
     <div className="relative lg:h-screen md:h-screen min-h-screen bg-[url('./src/images/manila-v5.jpg')] dark:bg-[url('./src/images/manila-v4.jpg')] bg-cover bg-no-repeat bg-top">
@@ -235,7 +240,7 @@ const LandingPageForm = () => {
             <div className="relative z-0 w-full mb-6 group">
              
               <input
-                type="password"
+                type={showPassword ? 'text' : 'password'}
                 id="user_pass"
                 name="user_pass"
                 placeholder=' '
@@ -244,6 +249,22 @@ const LandingPageForm = () => {
                 onChange={handleChange}
               />
               <label htmlFor="user_pass" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+              <button
+                type="button"
+                className="absolute right-3 top-4 cursor-pointer"
+                onClick={handleTogglePassword}
+              >
+                {showPassword ? (
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 dark:text-black">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                  </svg>
+                ) : (
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 dark:text-black">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                  </svg>
+                )}
+              </button>
             </div>
             )}
           </div>
