e2f901cb375039100ed208f46bac425c618efb1d
Mhndmts <ymhandeelouise.matis@tup.edu.ph>
2024-01-20 22:57:14 +0800

    AdminDB

--- backend/routes/connection1.js
+++ backend/routes/connection1.js
@@ -1,21 +1,21 @@
 import mysql from "mysql";
 
-const conn2 = mysql.createConnection({
+const conn1 = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
     password: "admin",
-    database: "clientdatabase",
+    database: "admindatabase",
   });
   
-  conn2.connect((err) => {
+  conn1.connect((err) => {
     if (err) {
       console.error("Error connecting to the database: " + err);
       return;
     }
-    console.log("Connected to the first database");
+    console.log("Connected to the second database");
   });
 
 
-export default conn2;
+export default conn1;
 
 
