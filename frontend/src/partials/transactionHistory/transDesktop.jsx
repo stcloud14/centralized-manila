c6d04b0b71ec30c7c5546871fe741505c5dcfb75
Miaaaaah <jeremiahpaul612@gmail.com>
2024-01-22 20:53:51 +0800

    transDesktop

--- frontend/src/partials/transactionHistory/transDesktop.jsx
+++ frontend/src/partials/transactionHistory/transDesktop.jsx
@@ -279,9 +279,11 @@ const TransDesktop = ({ searchInput, handleSearch, handleSearchInputChange, hand
                       <option value="SELECTSTATUS" className="text-slate-700 bg-white dark:text-slate-200 dark:bg-[#3d3d3d]">Select Status</option>
                       <option value="PENDING" className="bg-yellow-200 text-yellow-800">Pending</option>
                       <option value="PAID" className="bg-green-200 text-green-800">Paid</option>
-                      <option value="COMPLETE" className="bg-blue-200 text-blue-800">Complete</option>
+                      <option value="PROCESSING" className="bg-blue-200 text-blue-800">Processing</option>
+                      <option value="COMPLETE" className="bg-pink-200 text-pink-800">Complete</option>
                       <option value="REJECTED" className="text-red-800 bg-red-200">Rejected</option>
                       <option value="CANCELED" className="bg-slate-200 text-slate-800">Canceled</option>
+                      <option value="EXPIRED" className="bg-orange-200 text-orange-800">Expired</option>
                     </select>
                 </div>
 
