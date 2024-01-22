b16786fff3d7da0261a0c8e3381dc5d6127df802
Mhndmts <ymhandeelouise.matis@tup.edu.ph>
2024-01-22 20:14:51 +0800

    Normalization

--- frontend/src/partials/profile/CivilStatusDropdown.jsx
+++ frontend/src/partials/profile/CivilStatusDropdown.jsx
@@ -4,10 +4,10 @@ import React from 'react';
         return (
             <>
            <option value="0" className='dark:bg-[#3d3d3d]'>Select Civil Status</option>
-           <option value="SINGLE" className='dark:bg-[#3d3d3d]'>Single</option>
-           <option value="MARRIED" className='dark:bg-[#3d3d3d]'>Married</option>
-           <option value="SEPARATED" className='dark:bg-[#3d3d3d]'>Separated</option>
-           <option value="WIDOWED" className='dark:bg-[#3d3d3d]'>Widowed</option>
+           <option value="1" className='dark:bg-[#3d3d3d]'>Single</option>
+           <option value="2" className='dark:bg-[#3d3d3d]'>Married</option>
+           <option value="3" className='dark:bg-[#3d3d3d]'>Separated</option>
+           <option value="4" className='dark:bg-[#3d3d3d]'>Widowed</option>
             </>
 
             );
