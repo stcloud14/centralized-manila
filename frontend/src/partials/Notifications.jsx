0bf012b1687e8ed64ee89cd73773b88b8530128c
Miaaaaah <jeremiahpaul612@gmail.com>
2024-01-23 12:05:44 +0800

    Notifications

--- frontend/src/partials/Notifications.jsx
+++ frontend/src/partials/Notifications.jsx
@@ -62,7 +62,7 @@ function Notifications({ notifications }) {
               </span>
 
                 {notif.is_read === 0 ?
-                <svg xmlns="http://www.w3.org/2000/svg" fill="#0866ff" viewBox="0 0 24 24" stroke-width="1.5" stroke="#0866ff" class="w-3 h-3 mr-1.5">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#0866ff" viewBox="0 0 24 24" stroke-width="1.5" stroke="#0866ff" class="w-3 h-3 mr-1.5" style={{ flexShrink: 0 }}>
                   <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                 </svg>
                 : null}
