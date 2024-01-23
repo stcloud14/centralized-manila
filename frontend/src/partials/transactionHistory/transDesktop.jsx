ef409652b807f48f857186c64a3092c24c55bd49
Miaaaaah <jeremiahpaul612@gmail.com>
2024-01-23 12:16:24 +0800

    button

--- frontend/src/partials/transactionHistory/transDesktop.jsx
+++ frontend/src/partials/transactionHistory/transDesktop.jsx
@@ -327,10 +327,9 @@ const TransDesktop = ({ searchInput, handleSearch, handleSearchInputChange, hand
                     </select>
                 </div>
 
-                <button type="button" onClick={toggleDropdown} className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-1 mt-1 mb-0.5 rounded-full flex items-center ml-auto">
-
-                    <span className="">Filter</span>
-                  </button>
+                <button type="button" onClick={toggleDropdown} className="w-[6rem] bg-blue-500 hover:bg-blue-600 text-white px-4 py-1 mt-1 mb-0.5 rounded-full flex items-center ml-auto">
+                  <span className="mx-auto">Filter</span>
+                </button>
           </div>
         )}
                 </div>
