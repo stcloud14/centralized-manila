c12a36876fbf387253cab76901be1e7326a30485
stcloud14 <adrianpaolomaghirang@gmail.com>
2024-01-27 20:33:40 +0800

    ADMIN audit trails minor ui

--- frontend/src/admin_partials/audit_trail/auditMobile.jsx
+++ frontend/src/admin_partials/audit_trail/auditMobile.jsx
@@ -168,7 +168,7 @@ const AuditMobile = ({ searchInput, handleSearch, handleSearchInputChange, handl
         )}
       </div>
                     
-                  <button onClick={handleClearFilter} className="bg-slate-500 hover:bg-slate-600 text-white px-2.5 py-1 mr-2 rounded-sm group flex justify-center w-full items-center text-center">
+                  <button onClick={handleClearFilter} className="bg-slate-500 hover:bg-slate-600 text-white px-2.5 py-1 rounded-sm group flex justify-center w-full items-center text-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mr-1">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
                     </svg>
