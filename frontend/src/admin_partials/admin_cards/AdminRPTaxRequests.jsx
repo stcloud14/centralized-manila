9af2ec34f0be8a1c0e57e8db0c949db9376feeb6
Miaaaaah <jeremiahpaul612@gmail.com>
2024-01-29 19:22:38 +0800

    ADMIN FILTER FINAL NA YAN PLS

--- frontend/src/admin_partials/admin_cards/AdminRPTaxRequests.jsx
+++ frontend/src/admin_partials/admin_cards/AdminRPTaxRequests.jsx
@@ -10,7 +10,7 @@ import Loading from '../../partials/Loading';
 
 
 const AdminRPTaxRequests = ({ taxPayment, taxClearance, handleUpdateData }) => {
-
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
   const [selectedDate, setSelectedDate] = useState('');
   const [selectedDatee, setSelectedDatee] = useState('');
 
@@ -39,6 +39,10 @@ const AdminRPTaxRequests = ({ taxPayment, taxClearance, handleUpdateData }) => {
   const filteredTaxClearance = taxClearance.filter(handleSearch);
 
   const filteredTaxPayment = taxPayment.filter(handleSearch);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
 
   const handleToggleView = (mode) => {
     setViewMode(mode);
@@ -262,129 +266,166 @@ const AdminRPTaxRequests = ({ taxPayment, taxClearance, handleUpdateData }) => {
               </div>
               )}
 
-            {/* Search */}
-            {/* First Row */}
-            <div className="flex flex-col items-center md:flex-row text-xs pb-2">
+          {/* Search */}
+          <div className="flex flex-col items-center sm:flex-row text-xs pb-5">
+            <div className="flex-row flex justify-end w-full">
+            {/* Filter Button */}
+            <div className="relative w-full sm:w-20 text-left z-10">
+                <button type="button" onClick={toggleDropdown} className="bg-blue-500 hover:bg-blue-600 text-white justify-center py-1 mr-2 w-full rounded-sm inline-flex items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                    <path className="stroke-white" strokeLinecap="round" strokeLinejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
+                </svg>
+                  <span className="pl-1">Filter</span>
+                </button>
+
+              {isDropdownOpen && (
+              <div className="absolute w-[270px] origin-top-right py-2 px-3 bg-white dark:bg-[#212121] dark:text-slate-400 rounded-sm shadow-2xl z-20 md:right-10 sm:w-[405px]">
+
+              {/* Date Row */}
+              <div className="flex justify-center sm:justify-between items-center pb-[6px] sm:pb-[8px]">
+                  <span className="hidden sm:block text-xs">Date:</span>
+                  <span>
+                  <Flatpickr
+                      value={selectedDate}
+                      onChange={(date) => setSelectedDate(date[0])}
+                      options={{
+                      dateFormat: 'Y-m-d',
+                      altInput: true,
+                      altFormat: 'F j, Y',
+                      appendTo: document.body,
+                      onOpen: function (selectedDates, dateStr, instance) {
+                          if (document.documentElement.classList.contains('dark')) {
+                          const monthDropdown = instance.calendarContainer.querySelector(
+                              '.flatpickr-monthDropdown-months'
+                          );
+                          if (monthDropdown) {
+                              monthDropdown.style.backgroundColor = '#212121';
+                          }
+                          }
+                      },
+                      onClose: function (selectedDates, dateStr, instance) {
+                          const monthDropdown = instance.calendarContainer.querySelector(
+                          '.flatpickr-monthDropdown-months'
+                          );
+                          if (monthDropdown) {
+                          monthDropdown.style.backgroundColor = '';
+                          }
+                      },
+                      }}
+                      placeholder="From"
+                      className="bg-transparent text-xs border border-slate-300 text-slate-700 dark:text-white py-1 md:py-0.5 rounded-sm w-[110px] sm:w-[150px]"
+                  />
+                  <span> - </span>
+                  <Flatpickr
+                      value={selectedDatee}
+                      onChange={(date) => setSelectedDatee(date[0])}
+                      options={{
+                      dateFormat: 'Y-m-d',
+                      altInput: true,
+                      altFormat: 'F j, Y',
+                      appendTo: document.body,
+                      onOpen: function (selectedDates, dateStr, instance) {
+                          if (document.documentElement.classList.contains('dark')) {
+                          const monthDropdown = instance.calendarContainer.querySelector(
+                              '.flatpickr-monthDropdown-months'
+                          );
+                          if (monthDropdown) {
+                              monthDropdown.style.backgroundColor = '#212121';
+                          }
+                          }
+                      },
+                      onClose: function (selectedDates, dateStr, instance) {
+                          const monthDropdown = instance.calendarContainer.querySelector(
+                          '.flatpickr-monthDropdown-months'
+                          );
+                          if (monthDropdown) {
+                          monthDropdown.style.backgroundColor = '';
+                          }
+                      },
+                      }}
+                      placeholder="To"
+                      className="bg-transparent text-xs border border-slate-300 text-slate-700 dark:text-white py-1 md:py-0.5 rounded-sm w-[110px] sm:w-[150px]"
+                  />
+                  </span>
+              </div>
+
+
+              {/* Type Row */}
+              <div className="flex justify-center sm:justify-between items-center pb-[6px] sm:pb-[8px]">
+                  <span className="hidden sm:block text-xs">Type:</span>
+                  <select  value="" onChange="" name=""  id=""  className="text-xs border bg-transparent border-slate-300 text-slate-700 dark:text-white pl-4 rounded-sm peer cursor-pointer py-1 md:py-0.5 w-[235px]">
+                    <option value="All" className="dark:bg-[#3d3d3d]">Select Type</option>
+                    <option value="Real Property Tax Payment" className="dark:bg-[#3d3d3d]">Real Property Tax Payment</option>
+                    <option value="Real Property Tax Clearance" className="dark:bg-[#3d3d3d]">Real Property Tax Clearance</option>
+                  </select>
+              </div>
+
 
               {/* Transaction ID */}
-              <div className="relative flex flex-row sm:flex-col items-start w-full sm:pr-4 mb-[0.5rem] sm:mb-0">
-              <span className="hidden sm:block pr-1 pb-1 text-slate-700 dark:text-white">Transaction ID:</span>
-                <span className="absolute inset-y-0 left-0 pl-2 flex items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 ml-0 sm:mt-[1.2rem]">
-                    <path className='stroke-slate-400 dark:stroke-white' strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                  </svg>
-                </span>
-                <input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value.toUpperCase())} id="searchInput" type="text" placeholder="Search Transaction ID..." className="bg-transparent text-xs md:text-sm w-full border border-slate-300 text-slate-700 dark:text-white pl-8 py-1 md:py-0.5 rounded-sm"/>
-              </div>
-
-              {/* Transaction Type */}
-              <div className="flex flex-row sm:flex-col items-start w-full sm:pr-4 mb-[0.5rem] sm:mb-0">
-                <span className="hidden sm:block pr-1 pb-1 pt-[0.25rem] sm:pt-0 text-slate-700 dark:text-white">Type:</span>
-                <select onChange="" value="" name="" id="" className="text-xs border bg-transparent border-slate-300 text-slate-700 dark:text-white pl-2 py-1 rounded-sm peer cursor-pointer w-full">
-                  <option value="SELECTSTATUS" className="dark:bg-[#3d3d3d]">Select Type</option>
-                  <option value="RPTAXPAYMENT" className="dark:bg-[#3d3d3d]">Real Property Tax Payment</option>
-                  <option value="RPTAXCLEARANCE" className="dark:bg-[#3d3d3d]">Real Property Tax Clearance</option>
-                </select>
-              </div>
-
-              {/* Date */}
-              <div className="flex flex-row sm:flex-col items-start w-full mb-[0.5rem]sm:mb-0">
-                <span className="hidden sm:block pr-1 pb-1 text-slate-700 dark:text-white">Date:</span>
-              <div className="flex sm:flex-row w-full">
-                <Flatpickr
-                  value={selectedDate}
-                  onChange={(date) => setSelectedDate(date[0])}
-                  options={{
-                    dateFormat: 'Y-m-d',
-                    altInput: true,
-                    altFormat: 'F j, Y',
-                    appendTo: document.body,
-                    onOpen: function (selectedDates, dateStr, instance) {
-                      if (document.documentElement.classList.contains('dark')) {
-                        const monthDropdown = instance.calendarContainer.querySelector('.flatpickr-monthDropdown-months');
-                        if (monthDropdown) {
-                          monthDropdown.style.backgroundColor = '#212121';
-                        }
-                      }
-                    },
-                    onClose: function (selectedDates, dateStr, instance) {
-                      const monthDropdown = instance.calendarContainer.querySelector('.flatpickr-monthDropdown-months');
-                      if (monthDropdown) {
-                        monthDropdown.style.backgroundColor = '';
-                      }
-                    },
-                  }}
-                  placeholder="From"
-                  className="bg-transparent text-xs md:text-sm w-full border border-slate-300 text-slate-700 dark:text-white pl-2 py-1 md:py-0.5 rounded-sm"
-                />
-                <span className="px-1 pt-1.5">-</span>
-                <Flatpickr
-                  value={selectedDatee}
-                  onChange={(date) => setSelectedDatee(date[0])}
-                  options={{
-                    dateFormat: 'Y-m-d',
-                    altInput: true,
-                    altFormat: 'F j, Y',
-                    appendTo: document.body,
-                    onOpen: function (selectedDates, dateStr, instance) {
-                      if (document.documentElement.classList.contains('dark')) {
-                        const monthDropdown = instance.calendarContainer.querySelector('.flatpickr-monthDropdown-months');
-                        if (monthDropdown) {
-                          monthDropdown.style.backgroundColor = '#212121';
-                        }
-                      }
-                    },
-                    onClose: function (selectedDates, dateStr, instance) {
-                      const monthDropdown = instance.calendarContainer.querySelector('.flatpickr-monthDropdown-months');
-                      if (monthDropdown) {
-                        monthDropdown.style.backgroundColor = '';
-                      }
-                    },
-                  }}
-                  placeholder="To"
-                  className="bg-transparent text-xs md:text-sm w-full border border-slate-300 text-slate-700 dark:text-white pl-2 py-1 md:py-0.5 rounded-sm"
-                />
+              <div className="flex justify-center sm:justify-between items-center pb-[6px] sm:pb-[8px]">
+                  <span className="hidden sm:block pr-10 text-xs">
+                  Transaction ID:
+                  </span>
+                <div className="relative flex items-center">
+                  <span className="absolute inset-y-0 left-0 pl-2 flex items-center">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                      <path className='stroke-slate-400 dark:stroke-white' strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                      </svg>
+                  </span>
+                  <input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value.toUpperCase())} id="searchInput" type="text" placeholder="Search ID..." className="bg-transparent text-xs w-[235px] sm:w-[210px] border border-slate-300 text-slate-700 dark:text-white pl-8 py-1 md:py-0.5 rounded-sm"/>
                 </div>
-              </div> 
+              </div>
+
+                {/* TIN */}
+                <div className="flex justify-center sm:justify-between items-center pb-[6px] sm:pb-[8px]">
+                  <span className="hidden sm:block pr-10 text-xs">
+                  TIN:
+                  </span>
+                <div className="relative flex items-center">
+                  <span className="absolute inset-y-0 left-0 pl-2 flex items-center">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                      <path className='stroke-slate-400 dark:stroke-white' strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                      </svg>
+                  </span>
+                  <input value="" onChange="" id="searchInput" type="text" placeholder="Search TIN..." className="bg-transparent text-xs w-[235px] sm:w-[210px] border border-slate-300 text-slate-700 dark:text-white pl-8 py-1 md:py-0.5 rounded-sm"/>
+                </div>
+              </div>
+
+                {/* Transaction ID Row */}
+                <div className="flex justify-center sm:justify-between items-center pb-[6px] sm:pb-[8px]">
+                  <span className="hidden sm:block pr-10 text-xs">
+                  PIN
+                  </span>
+                <div className="relative flex items-center">
+                  <span className="absolute inset-y-0 left-0 pl-2 flex items-center">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                      <path className='stroke-slate-400 dark:stroke-white' strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                      </svg>
+                  </span>
+                  <input value="" onChange="" id="searchInput" type="text" placeholder="Search PIN..." className="bg-transparent text-xs w-[235px] sm:w-[210px] border border-slate-300 text-slate-700 dark:text-white pl-8 py-1 md:py-0.5 rounded-sm"/>
+                </div>
+              </div>
+
+              <button type="button" onClick="" className=" bg-blue-500 hover:bg-blue-600 text-white mr-[6px] sm:mr-[0px] px-4 py-1 mt-2 mb-0.5 rounded-sm flex items-center ml-auto">
+                  <span className="mx-auto">Filter</span>
+              </button>
+              </div>
+              )}
             </div>
 
-              {/* 2nd Filter Row */}
-          <div className="flex flex-col items-center md:flex-row text-xs pb-5">
-
-              {/* Tax Identification Number */}
-              <div className="relative flex flex-row sm:flex-col items-start w-full sm:pr-4 mb-[0.5rem] sm:mb-0">
-              <span className="hidden sm:block  pr-1 pb-1 text-slate-700 dark:text-white">Tax Identification Number:</span>
-                <span className="absolute inset-y-0 left-0 pl-2 flex items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 sm:mt-[1.2rem]">
-                    <path className='stroke-slate-400 dark:stroke-white' strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                  </svg>
-                </span>
-                <input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value.toUpperCase())} id="searchInput" type="text" placeholder="Search TIN..." className="bg-transparent text-xs md:text-sm w-full border border-slate-300 text-slate-700 dark:text-white pl-8 py-1 md:py-0.5 rounded-sm"/>
-              </div>
-
-              {/* Property Identification Number */}
-              <div className="relative flex flex-row sm:flex-col items-start w-full sm:pr-4 mb-[0.5rem] sm:mb-0">
-              <span className="hidden sm:block pr-1 pb-1 text-slate-700 dark:text-white">Property Identification Number:</span>
-                <span className="absolute inset-y-0 left-0 pl-2 flex items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 sm:mt-[1.2rem]">
-                    <path className='stroke-slate-400 dark:stroke-white' strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                  </svg>
-                </span>
-                <input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value.toUpperCase())} id="searchInput" type="text" placeholder="Search PIN..." className="bg-transparent text-xs md:text-sm w-full border border-slate-300 text-slate-700 dark:text-white pl-8 py-1 md:py-0.5 rounded-sm"/>
-              </div>
-
-              {/* Clear Button */}
-              <div className="flex w-full sm:w-32 items-center justify-center sm:mt-[1.3rem]">
-                <button onClick="" className="bg-slate-500 hover:bg-slate-600 text-white px-4 py-1 w-full rounded-sm inline-flex justify-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mr-0.5">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-                  </svg>
-                  <span>&nbsp;Clear</span>
-                </button>
-              </div>
+            {/* Clear Button */}
+            <div className="w-full sm:w-20 ml-2">
+            <button type="button" onClick="" className="bg-slate-500 hover:bg-slate-600 text-white justify-center py-1 w-full rounded-sm inline-flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+              </svg>
+                <span className="pl-1">Clear</span>
+            </button>
+            </div>
+            </div>
 
               {/* View Toggle */}
-            <div className="flex items-center text-xs ml-2 mt-[1.3rem]">
+            <div className="flex items-center text-xs sm:ml-2 mt-5 sm:mt-0">
               <div className="relative flex items-center">
                 {/* Tabular View Toggle */}
                 <button onClick={() => handleToggleView('table')}  className='flex items-center p-1 text-slate-500 hover:text-black dark:text-slate-400 dark:hover:text-white'>
