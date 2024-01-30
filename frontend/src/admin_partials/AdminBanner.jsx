f1edebb09632c9dffab25bdf1a68a3e7f41a6543
Miaaaaah <jeremiahpaul612@gmail.com>
2024-01-30 16:02:01 +0800

    Admin Dashboard Date Filter

--- frontend/src/admin_partials/AdminBanner.jsx
+++ frontend/src/admin_partials/AdminBanner.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Flatpickr from 'react-flatpickr';
 
 function AdminBanner({ adminType, generateReports}) {
+  const [selectedDate, setSelectedDate] = useState('');
+  const [selectedDatee, setSelectedDatee] = useState('');
 
   const admin = (() => {
     switch (adminType) {
@@ -37,12 +40,81 @@ function AdminBanner({ adminType, generateReports}) {
           <p className="lg:text-sm dark:text-slate-400 font-thin text-xs">Discover what's happening today.</p>
         </div>
         
-        <button onClick={generateReports} className="border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white md:text-xs text-[0.60rem] py-2 px-4 rounded-sm flex items-center">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="hidden md:inline-block w-5 h-5 mr-2">
+
+        <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center">
+  <div className="mb-2 sm:mb-0 flex items-center">
+    <span className="hidden sm:block text-xs mr-2">Date:</span>
+    <Flatpickr
+      value={selectedDate}
+      onChange={(date) => setSelectedDate(date[0])}
+      options={{
+        dateFormat: 'Y-m-d',
+        altInput: true,
+        altFormat: 'F j, Y',
+        appendTo: document.body,
+        onOpen: function (selectedDates, dateStr, instance) {
+          if (document.documentElement.classList.contains('dark')) {
+            const monthDropdown = instance.calendarContainer.querySelector(
+              '.flatpickr-monthDropdown-months'
+            );
+            if (monthDropdown) {
+              monthDropdown.style.backgroundColor = '#212121';
+            }
+          }
+        },
+        onClose: function (selectedDates, dateStr, instance) {
+          const monthDropdown = instance.calendarContainer.querySelector(
+            '.flatpickr-monthDropdown-months'
+          );
+          if (monthDropdown) {
+            monthDropdown.style.backgroundColor = '';
+          }
+        },
+      }}
+      placeholder="From"
+      className="bg-white dark:bg-[#2b2b2b] text-xs border border-slate-300 text-slate-700 dark:text-white py-1 md:py-0.5 rounded-sm w-[110px] sm:w-[150px]"
+    />
+    <span className="mx-2"> - </span>
+    <Flatpickr
+      value={selectedDatee}
+      onChange={(date) => setSelectedDatee(date[0])}
+      options={{
+        dateFormat: 'Y-m-d',
+        altInput: true,
+        altFormat: 'F j, Y',
+        appendTo: document.body,
+        onOpen: function (selectedDates, dateStr, instance) {
+          if (document.documentElement.classList.contains('dark')) {
+            const monthDropdown = instance.calendarContainer.querySelector(
+              '.flatpickr-monthDropdown-months'
+            );
+            if (monthDropdown) {
+              monthDropdown.style.backgroundColor = '#212121';
+            }
+          }
+        },
+        onClose: function (selectedDates, dateStr, instance) {
+          const monthDropdown = instance.calendarContainer.querySelector(
+            '.flatpickr-monthDropdown-months'
+          );
+          if (monthDropdown) {
+            monthDropdown.style.backgroundColor = '';
+          }
+        },
+      }}
+      placeholder="To"
+      className="bg-white dark:bg-[#2b2b2b] text-xs border border-slate-300 text-slate-700 dark:text-white py-1 md:py-0.5 rounded-sm w-[110px] sm:w-[150px]"
+    />
+
+  </div>
+  
+  <button onClick={generateReports} className="border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white md:text-xs text-[0.60rem] py-2 px-4 sm:ml-10 w-auto rounded-sm flex items-center">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 mr-2 flex-shrink-0">
             <path fillRule="evenodd" d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 0 0 3 3h.27l-.155 1.705A1.875 1.875 0 0 0 7.232 22.5h9.536a1.875 1.875 0 0 0 1.867-2.045l-.155-1.705h.27a3 3 0 0 0 3-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0 0 18 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25ZM16.5 6.205v-2.83A.375.375 0 0 0 16.125 3h-8.25a.375.375 0 0 0-.375.375v2.83a49.353 49.353 0 0 1 9 0Zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 0 1-.374.409H7.232a.375.375 0 0 1-.374-.409l.526-5.784a.373.373 0 0 1 .333-.337 41.741 41.741 0 0 1 8.566 0Zm.967-3.97a.75.75 0 0 1 .75-.75h.008a.75.75 0 0 1 .75.75v.008a.75.75 0 0 1-.75.75H18a.75.75 0 0 1-.75-.75V10.5ZM15 9.75a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75V10.5a.75.75 0 0 0-.75-.75H15Z" clipRule="evenodd" />
           </svg>
           Generate Reports
         </button>
+        </div>
       </div>
     </div>
   );
