338cacf97a284a21c4a3924f1e9de7cbc7dcf495
milesluna <milesjohnluna@gmail.com>
2024-01-30 13:40:53 +0800

    busmodal and trnshandler

--- frontend/src/partials/transactionModal/BusinessModal.jsx
+++ frontend/src/partials/transactionModal/BusinessModal.jsx
@@ -797,18 +797,15 @@ const handleCloseConfirm = () => {
 
            {/* QR Code Section */}
            <div className="bg-white dark:bg-[#212121] text-slate-700 dark:text-white px-4 pt-3 pb-5 gap-3 sm:px-6 flex items-center justify-between rounded-b-lg">
-                <div className="whitespace-nowrap md:mb-0 mb-1">
-                    {businessTransaction ? (
-                        // Automatically redirect to the download link when the QR code is clicked
-                        <a href={generateDownloadLink(businessTransaction)} target="_blank" rel="noreferrer">
-                            <QRCode value={generateDownloadLink(businessTransaction)} size={100} />
-                        </a>
-                    ) : (
-                        <Loading />
-                    )}
-                </div>
-            </div>
-                 
+                      <div className="whitespace-nowrap md:mb-0 mb-1">
+                          {businessTransaction ? (
+                              // Display the QR code without the anchor tag
+                              <QRCode value={generateDownloadLink(businessTransaction)} size={100} />
+                          ) : (
+                              <Loading />
+                          )}
+                      </div>
+                  </div>
 
 
             <div className="flex items-center space-x-2 ml-auto">
