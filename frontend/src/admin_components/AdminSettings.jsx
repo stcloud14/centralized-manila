27aeb61a874f01496e41786a89a3de78cc931871
Miaaaaah <jeremiahpaul612@gmail.com>
2024-05-05 18:59:48 +0800

    agfdsfgdfgs

--- frontend/src/admin_components/AdminSettings.jsx
+++ frontend/src/admin_components/AdminSettings.jsx
@@ -369,14 +369,17 @@ const AdminSettings =()=>{
                       </div>
                     </div>
                     <h1 className='font-medium text-center text-slate-700 dark:text-white'>Change Password</h1>
-                    <div className="flex items-center justify-center w-full bg-slate-100 rounded-full py-2 text-center mb-2">
-                      <h1 className="font-medium text-sm text-slate-700 ">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 mr-2 inline-block text-slate-700 ">
+                      <h1 className="font-medium text-center text-sm text-slate-700 dark:text-white ">
+                        Contact the system admin to change your password.
+                      </h1>
+                    <button className="flex items-center justify-center w-full  text-slate-500 hover:text-white border border-slate-500 hover:bg-slate-500 focus:ring-4 focus:outline-none focus:ring-slate-300 font-normal rounded-full text-sm py-1.5 text-center mb-2 dark:border-slate-200 dark:text-slate-200 dark:hover:text-slate-700 dark:hover:bg-slate-200 dark:focus:ring-slate-500">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-5 h-5 mr-2 inline-block">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z"/>
                         </svg>
-                        Contact the System Admin to Change Your Password.
+                        <h1 className="font-medium text-sm ">
+                      Change Password
                       </h1>
-                    </div>
+                    </button>
                     {/* <h1 className='font-medium text-center text-slate-700 dark:text-white'>Password</h1>
                     <div className='mb-0'>
                       <h1 className='italic text-xs'>Password must be:</h1>
