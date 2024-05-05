a7b0b6e22ade703bd739b05383b313dd4081c2cf
Miaaaaah <jeremiahpaul612@gmail.com>
2024-05-05 18:27:47 +0800

    Change Pass Message Admin

--- frontend/src/admin_components/AdminSettings.jsx
+++ frontend/src/admin_components/AdminSettings.jsx
@@ -370,7 +370,6 @@ const AdminSettings =()=>{
                     </div>
                     <h1 className='font-medium text-center text-slate-700 dark:text-white'>Change Password</h1>
                     <div className="flex items-center justify-center w-full bg-slate-200 rounded-full py-2 text-center mb-2">
-                      
                       <h1 className="font-medium text-sm text-slate-700 ">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 mr-2 inline-block text-slate-700 ">
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M15.75 5.25a3 3 0 0 1 3 3m3 0a6 6 0 0 1-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1 1 21.75 8.25Z"/>
