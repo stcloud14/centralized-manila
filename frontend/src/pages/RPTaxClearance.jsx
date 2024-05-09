51cc902a98961c9fec9fd1714333d7ba1ac79127
ruficarl <107566254+flaucirr@users.noreply.github.com>
2024-05-09 13:22:46 +0800

    FIXING NAME

---
+++ frontend/src/pages/RPTaxClearance.jsx
@@ -0,0 +1,9 @@
+import RPTaxClearanceForm from "../components/RPTaxClearanceForm";
+
+function RPTaxClearance() {
+  return (  
+    <RPTaxClearanceForm />
+  );
+}
+
+export default RPTaxClearance;
