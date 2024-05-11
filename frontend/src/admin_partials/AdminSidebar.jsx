a3dd08ab4dfcb28ee316a5d239dc9d4186ebcd99
Miaaaaah <jeremiahpaul612@gmail.com>
2024-05-11 17:08:34 +0800

    sidebar rename

--- frontend/src/admin_partials/AdminSidebar.jsx
+++ frontend/src/admin_partials/AdminSidebar.jsx
@@ -383,7 +383,7 @@ function AdminSidebar({ sidebarOpen, setSidebarOpen }) {
                               }
                             >
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                                Charges Section
+                                Permit Charges
                               </span>
                             </NavLink>
                           </li>
