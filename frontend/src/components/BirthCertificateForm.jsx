eb39e2ff4d7530408291b7dcaac1b9d18207d54c
milesluna <milesjohnluna@gmail.com>
2024-05-12 12:27:26 +0800

    birtc user

--- frontend/src/components/BirthCertificateForm.jsx
+++ frontend/src/components/BirthCertificateForm.jsx
@@ -24,6 +24,7 @@ import BCTermsModal from '../partials/business/BCTermsModal';
 import ModalTransaction from '../partials/transactionModal/ModalTransaction';
 import TermsModal from '../partials/business/TermsModal';
 import VerifyModal from '../partials/business/VerifyModal';
+import CountryDropdwon from '../partials/profile/CountryDropdown';
 
 const BirthCertificateForm =()=>{
 
@@ -784,8 +785,10 @@ const BirthCertificateForm =()=>{
                 {/* Row 1 */}
                 <div className="grid md:grid-cols-2 md:gap-6">
                   <div className="relative z-0 w-full mb-6 group">
-                    <input onChange={handleInputChange} value={birthCert.birthc_country} type="text" name="birthc_country" id="birthc_country" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
-                    <label htmlFor="birthc_country" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Specify Country ONLY if Born Abroad</label>
+                    <select onChange={handleInputChange} value={birthCert.birthc_country} name="birthc_country" id="birthc_country" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer cursor-pointer" placeholder="" required>
+                  <CountryDropdwon />
+                  </select>
+                  <label htmlFor="birthc_country" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Specify Country ONLY if Born Abroad</label>
                   </div>
                   <div className="relative z-0 w-full mb-6 group">
                     <input onChange={handleInputChange} value={birthCert.birthc_bren} type="text" name="birthc_bren" id="birthc_bren" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "/>
