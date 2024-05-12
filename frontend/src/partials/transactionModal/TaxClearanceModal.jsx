5505e1666283e25c1dfb8be893500fe94823d493
Frostoverthesheep <atanaciobasa9@gmail.com>
2024-05-12 17:12:51 +0800

    loading

--- frontend/src/partials/transactionModal/TaxClearanceModal.jsx
+++ frontend/src/partials/transactionModal/TaxClearanceModal.jsx
@@ -335,7 +335,11 @@ const cancelTrans = async (e) => {
                         <hr className='mt-7 mb-1'/>
                         <div className="flex justify-between">
                         <span className="font-semibold whitespace-nowrap">{status_type === "Pending" ? "Amount Paid" : status_type === "Processing" ? "Amount to Pay" : "Amount"}</span>
-                          <span className="font-semibold whitespace-nowrap ml-4">P {taxClearanceTransaction.amount ? taxClearanceTransaction.amount + '.00': '-'}</span>
+                        <span className="font-semibold whitespace-nowrap ml-4"> 
+                        {taxClearanceTransaction && status_type ? (
+                          `P {taxClearanceTransaction.amount ? taxClearanceTransaction.amount + '.00': '-'}`
+                          ) : `Submit for amount declaration.`}
+                          </span>
                         </div>
                       </div>
                     </div>
