6279027b151821d429239761d6c4562c53878425
milesluna <milesjohnluna@gmail.com>
2024-05-13 16:21:48 +0800

    valid id dropdown

--- frontend/src/partials/profile/ValidIdDropdown.jsx
+++ frontend/src/partials/profile/ValidIdDropdown.jsx
@@ -3,18 +3,61 @@ import React from 'react';
     const ValidIdDropdown = () => {
         return (
             <>
-                      <option value="0" className='dark:bg-[#3d3d3d]'>Select Valid ID to Present Upon Claiming</option>
-                      <option value="1" className='dark:bg-[#3d3d3d]'>PASSPORT</option>
-                      <option value="2" className='dark:bg-[#3d3d3d]'>SSS</option>
-                      <option value="3" className='dark:bg-[#3d3d3d]'>UMID</option>
-                      <option value="4" className='dark:bg-[#3d3d3d]'>PHILHEALTH</option>
-                      <option value="5" className='dark:bg-[#3d3d3d]'>DRIVER'S LICENSE</option>
-                      <option value="6" className='dark:bg-[#3d3d3d]'>VOTER'S ID</option>
-                      <option value="7" className='dark:bg-[#3d3d3d]'>SENIOR CITIZEN'S ID</option>
-                      <option value="8" className='dark:bg-[#3d3d3d]'>POSTAL ID</option>
-                      <option value="9" className='dark:bg-[#3d3d3d]'>BARANGAY ID</option>
-                      <option value="10" className='dark:bg-[#3d3d3d]'>NATIONAL ID</option>
-                      <option value="11" className='dark:bg-[#3d3d3d]'>AUTHORIZATION LETTER</option>
+                    
+                    <option value="0" className='dark:bg-[#3d3d3d]'>Select Valid ID to Present Upon Claiming</option>
+                    <option value="1" className='dark:bg-[#3d3d3d]'>PASSPORT</option>
+                    <option value="2" className='dark:bg-[#3d3d3d]'>SSS</option>
+                    <option value="3" className='dark:bg-[#3d3d3d]'>UMID</option>
+                    <option value="4" className='dark:bg-[#3d3d3d]'>PHILHEALTH</option>
+                    <option value="5" className='dark:bg-[#3d3d3d]'>DRIVER'S LICENSE</option>
+                    <option value="6" className='dark:bg-[#3d3d3d]'>VOTER'S ID</option>
+                    <option value="7" className='dark:bg-[#3d3d3d]'>SENIOR CITIZEN'S ID</option>
+                    <option value="8" className='dark:bg-[#3d3d3d]'>POSTAL ID</option>
+                    <option value="9" className='dark:bg-[#3d3d3d]'>BARANGAY ID</option>
+                    <option value="10" className='dark:bg-[#3d3d3d]'>NATIONAL ID</option>
+                    <option value="11" className='dark:bg-[#3d3d3d]'>AUTHORIZATION LETTER</option>
+                    <option value="12" className='dark:bg-[#3d3d3d]'>GSIS ID</option>
+                    <option value="13" className='dark:bg-[#3d3d3d]'>OWWA ID</option>
+                    <option value="14" className='dark:bg-[#3d3d3d]'>PRC ID</option>
+                    <option value="15" className='dark:bg-[#3d3d3d]'>TIN ID</option>
+                    <option value="16" className='dark:bg-[#3d3d3d]'>PHILIPPINE PASSPORT CARD</option>
+                    <option value="17" className='dark:bg-[#3d3d3d]'>PHILIPPINE NATIONAL POLICE ID</option>
+                    <option value="18" className='dark:bg-[#3d3d3d]'>AFP ID</option>
+                    <option value="19" className='dark:bg-[#3d3d3d]'>IBP ID</option>
+                    <option value="20" className='dark:bg-[#3d3d3d]'>PRC ID</option>
+                    <option value="21" className='dark:bg-[#3d3d3d]'>DFA EPASSPORT</option>
+                    <option value="22" className='dark:bg-[#3d3d3d]'>OFW ID</option>
+                    <option value="23" className='dark:bg-[#3d3d3d]'>SSS UMID</option>
+                    <option value="24" className='dark:bg-[#3d3d3d]'>DFA ISSUED POSTAL ID</option>
+                    <option value="25" className='dark:bg-[#3d3d3d]'>DOLE ID</option>
+                    <option value="26" className='dark:bg-[#3d3d3d]'>NCWDP ID</option>
+                    <option value="27" className='dark:bg-[#3d3d3d]'>DSWD ID</option>
+                    <option value="28" className='dark:bg-[#3d3d3d]'>PNR ID</option>
+                    <option value="29" className='dark:bg-[#3d3d3d]'>LGU ID</option>
+                    <option value="30" className='dark:bg-[#3d3d3d]'>PHILHEALTH INSURANCE CARD</option>
+                    <option value="31" className='dark:bg-[#3d3d3d]'>POEA eCARD</option>
+                    <option value="32" className='dark:bg-[#3d3d3d]'>NBI CLEARANCE</option>
+                    <option value="33" className='dark:bg-[#3d3d3d]'>CID CLEARANCE</option>
+                    <option value="34" className='dark:bg-[#3d3d3d]'>DTI CERTIFICATE</option>
+                    <option value="35" className='dark:bg-[#3d3d3d]'>COMPANY ID</option>
+                    <option value="36" className='dark:bg-[#3d3d3d]'>MARINA ID</option>
+                    <option value="37" className='dark:bg-[#3d3d3d]'>DOH LICENSE</option>
+                    <option value="38" className='dark:bg-[#3d3d3d]'>DOE LICENSE</option>
+                    <option value="39" className='dark:bg-[#3d3d3d]'>PRC PROFESSIONAL ID</option>
+                    <option value="40" className='dark:bg-[#3d3d3d]'>IBP ID</option>
+                    <option value="41" className='dark:bg-[#3d3d3d]'>IMMIGRANT CERTIFICATE</option>
+                    <option value="42" className='dark:bg-[#3d3d3d]'>BIR TIN ID</option>
+                    <option value="43" className='dark:bg-[#3d3d3d]'>LGU OFFICE ID</option>
+                    <option value="44" className='dark:bg-[#3d3d3d]'>PNP FIREARMS LICENSE</option>
+                    <option value="45" className='dark:bg-[#3d3d3d]'>SOLO PARENT ID</option>
+                    <option value="46" className='dark:bg-[#3d3d3d]'>PWD ID</option>
+                    <option value="47" className='dark:bg-[#3d3d3d]'>DOST ID</option>
+                    <option value="48" className='dark:bg-[#3d3d3d]'>PRC LICENSURE CERTIFICATE</option>
+                    <option value="49" className='dark:bg-[#3d3d3d]'>OFW eCARD</option>
+                    <option value="50" className='dark:bg-[#3d3d3d]'>DFA ID</option>
+                    <option value="51" className='dark:bg-[#3d3d3d]'>SCHOOL ID</option>
+
+
             </>
 
             );
