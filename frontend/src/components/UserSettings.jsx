f84d806ed543abf585f29dca19ad18133cc042b0
Frostoverthesheep <atanaciobasa9@gmail.com>
2024-05-18 01:51:12 +0800

    TransDesktop and TransMobile Fix

--- frontend/src/components/UserSettings.jsx
+++ frontend/src/components/UserSettings.jsx
@@ -632,7 +632,7 @@ useEffect(() => {
                     <img
                       name='userImage' 
                       className="inline-block h-72 w-72 rounded-full border-2 border-black dark:border-white p-1 object-cover object-center relative z-1"
-                      src={preSelectedFile || userImage || defaultImg}
+                      src={preSelectedFile || userImage }
                       alt="User Profile"
                       onError={(e) => {
                         console.error('Error loading image for user profile:', e);
@@ -669,7 +669,7 @@ useEffect(() => {
                           <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
                           <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z" />
                         </svg>
-                        { (preSelectedFile || userImage || defaultImg) === defaultImg ? 'Upload Profile Photo' : 'Change Profile Photo' }
+                        { (preSelectedFile || userImage ) === defaultImg ? 'Upload Profile Photo' : 'Change Profile Photo' }
                       </button>
                       ) : null}
 
@@ -698,7 +698,7 @@ useEffect(() => {
                             </button>
                           ) : null}
 
-                          { (preSelectedFile || userImage || defaultImg) !== defaultImg && isButtonVisible ? (
+                          { (preSelectedFile || userImage ) !== defaultImg && isButtonVisible ? (
                             <button
                               type="button"
                               onClick={handleRemoveImage}
