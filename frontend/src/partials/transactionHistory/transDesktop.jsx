3cdb13e9b81407234444ee0666566e831815d241
Frostoverthesheep <atanaciobasa9@gmail.com>
2024-05-18 00:59:17 +0800

    TransDesktop and TransMobile Fix

--- frontend/src/partials/transactionHistory/transDesktop.jsx
+++ frontend/src/partials/transactionHistory/transDesktop.jsx
@@ -12,9 +12,10 @@ import TransTypeDropdown from '../transDropdown/TransTypeDropdown';
 import StatusTypeDropdown from '../transDropdown/StatusTypeDropdown';
 import FilterButton from '../FilterButton';
 
-const TransMobile = ({ searchInput, setSearchInput, handleSearch, handleSearchInputChange, handleOpenModal, handleClearFilter, handleSortChange, sortOption, sortOrder, SortIcon, sortedTransactions, handleInputChange, handleInputChange2, selectedDate, setSelectedDate, selectedDatee, setSelectedDatee, selectedStatus, selectedType, filteredTransactions, userPersonal, soaData, isButtonDisabled }) => {
+const TransDesktop = ({ searchInput, setSearchInput, handleSearch, handleOpenModal, handleClearFilter, handleSortChange, sortOption, sortOrder, SortIcon, sortedTransactions, handleInputChange, handleInputChange2, selectedDate, setSelectedDate, selectedDatee, setSelectedDatee, selectedStatus, selectedType, filteredTransactions, userPersonal, soaData, isButtonDisabled }) => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
-
+  //const isButtonDisabled = ['Paid', 'Processing', 'Expired', 'Canceled', 'Complete', 'Rejected'].includes(selectedStatus) || filteredTransactions.length === 0;
+  const Base_Url = process.env.Base_Url;
   const formatAmount = (amount) => {
     const parsedAmount = parseFloat(amount);
     if (isNaN(parsedAmount)) {
@@ -30,6 +31,10 @@ const TransMobile = ({ searchInput, setSearchInput, handleSearch, handleSearchIn
   const toggleDropdown = () => {
     setDropdownOpen(!isDropdownOpen);
   };
+
+
+
+  console.log(filteredTransactions)
 
   const generateUniqueSOA = (type, user) => {
 
@@ -404,63 +409,71 @@ const TransMobile = ({ searchInput, setSearchInput, handleSearch, handleSearchIn
     }
 };
 
+    
+  
     return (
         <>
         <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-[#2b2b2b] dark:border-[#3d3d3d] shadow-lg rounded-sm border border-slate-200">
-              <div className="px-5 py-5">
-                <h1 className='font-medium text-center text-slate-700 dark:text-white mb-5'>Transaction History</h1>
-                <div className="flex flex-col items-center mb-2 md:flex-row md:px-0 md:pr-0.5 px-0.5 text-xs">
-                  <div className="flex w-full items-center">
-                    <button
-                      type="button"
-                      onClick={toggleDropdown}
-                      className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-1 mr-2 justify-center group w-full items-center flex text-center rounded-full"
-                    >
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
-                        <path className="stroke-white" strokeLinecap="round" strokeLinejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
-                      </svg>
-                      <span className="pl-1">Filter</span>
-                    </button>
+          <div className="px-5 py-5">
+            <h1 className='font-medium text-center text-slate-700 dark:text-white mb-7 md:mb-3'>Transaction History</h1>
+              <div className="flex items-center justify-end mb-2 md:px-0 md:pr-0.5 px-0.5 text-xs">
+                {/* <div className="relative mr-2">
+                  <span className="absolute inset-y-0 left-0 pl-2 flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                      <path className='stroke-slate-400 dark:stroke-white' strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                    </svg>
+                  </span>
+                  <input value={searchInput} onChange={(e) => handleSearch(e.target.value.toUpperCase())} id="searchInput" onKeyDown={(e) => e.key === 'Enter' && handleSearch()} type="text" placeholder="Search ID..." className="bg-transparent text-xs md:text-sm w-full md:w-80 border border-slate-300 text-slate-700 dark:text-white pl-8 py-1 md:py-0.5 rounded-full w-full md:w-auto"/>
+                </div> */}
+                
+                <div className="relative inline-block text-left z-10">
+                  <button type="button" onClick={toggleDropdown} className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-1 mr-2 rounded-full inline-flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
+                      <path className="stroke-white" strokeLinecap="round" strokeLinejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
+                  </svg>
+                    <span className="pl-1">Filter</span>
+                  </button>
 
                     {isDropdownOpen && (
                       <FilterButton 
-                        searchInput={searchInput} 
-                        setSearchInput={setSearchInput}
-                        handleSearch={handleSearch} 
-                        selectedDate={selectedDate}
-                        setSelectedDate={setSelectedDate}
-                        selectedDatee={selectedDatee}
-                        setSelectedDatee={setSelectedDatee}
-                        selectedStatus={selectedStatus}
-                        handleInputChange={handleInputChange}
-                        handleInputChange2={handleInputChange2}
-                        selectedType={selectedType}
-                        toggleDropdown={toggleDropdown}
+                      searchInput={searchInput} 
+                      setSearchInput={setSearchInput}
+                      handleSearch={handleSearch} 
+                      selectedDate={selectedDate}
+                      setSelectedDate={setSelectedDate}
+                      selectedDatee={selectedDatee}
+                      setSelectedDatee={setSelectedDatee}
+                      selectedStatus={selectedStatus}
+                      handleInputChange={handleInputChange}
+                      handleInputChange2={handleInputChange2}
+                      selectedType={selectedType}
+                      toggleDropdown={toggleDropdown}
                       />
                     )}
+                    
+                </div>
 
-                    <button onClick={handleClearFilter} className="bg-slate-500 hover:bg-slate-600 text-white px-4 py-1 justify-center w-full group items-center flex text-center rounded-full">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mr-0.5">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-                      </svg>
-                      <span>&nbsp;Clear</span>
-                    </button>
-                  </div>
+                <button onClick={handleClearFilter} className="bg-slate-500 hover:bg-slate-600 text-white px-4 py-1 mr-2 rounded-full inline-flex items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mr-0.5">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                  </svg>
+                  <span>&nbsp;Clear</span>
+                </button>
 
-                  <button
-                    className={`group border border-emerald-500 text-emerald-500 hover:bg-emerald-500 hover:text-white px-4 py-1 mt-2 rounded-full w-full justify-center inline-flex items-center ${isButtonDisabled ? 'opacity-50 cursor-not-allowed' : ''}`}
-                    onClick={generatePDF}
-                    disabled={isButtonDisabled}
-                  >
+                <button
+                  className={`group border border-emerald-500 text-emerald-500 hover:bg-emerald-500 hover:text-white px-4 py-1 rounded-full inline-flex items-center ${isButtonDisabled ? 'opacity-50 cursor-not-allowed' : ''}`}
+                  onClick={generatePDF}
+                  disabled={isButtonDisabled}
+                >
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                     <path className="stroke-emerald-500 group-hover:stroke-white" strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
                   </svg>
                   <span>&nbsp;Download SOA</span>
                 </button>
 
-                </div>
+              </div>
 
-            {/* <div className="relative overflow-x-auto shadow-md md:rounded-lg rounded-md">
+            <div className="relative overflow-x-auto shadow-md md:rounded-lg rounded-md">
               <table className="w-full text-left text-xs md:text-sm rtl:text-right text-gray-500 dark:text-gray-400">
                 <thead className="text-gray-700 uppercase bg-slate-200 dark:bg-[#212121] dark:text-slate-400">
                     <tr>
@@ -493,7 +506,7 @@ const TransMobile = ({ searchInput, setSearchInput, handleSearch, handleSearchIn
                             </div>
                         </th>
                         <th>
-                          
+                          {/* View Details*/}
                         </th>
                     </tr>
                 </thead>
@@ -547,60 +560,11 @@ const TransMobile = ({ searchInput, setSearchInput, handleSearch, handleSearchIn
                 )}
               </tbody>
               </table>
-            </div> */}
-
-              {filteredTransactions.length === 0 ? (
-                <div className="text-center text-sm py-4">
-                  {searchInput || selectedType || selectedStatus ? (
-                    <span>No records found.</span>
-                  ) : (
-                    <span>No records available.</span>
-                  )}
-                </div>
-              ) : (
-
-                filteredTransactions.map((transaction) => (
-                  <div key={transaction.transaction_id} className="bg-white dark:bg-[#333333] shadow-md rounded-md mb-4">
-                    <div className="text-xs font-semibold text-slate-60 bg-slate-200 dark:bg-[#212121] dark:text-white rounded-t-md px-4 py-1.5">
-                      Transaction Details
-                    </div>
-                    <div className="px-4 py-5">
-                      <div className="text-xs text-slate-600 dark:text-slate-300 my-1">
-                        <span className="font-semibold">Transaction ID:</span> {transaction.transaction_id}
-                      </div>
-                      <div className="text-xs text-slate-600 dark:text-slate-300 my-1">
-                        <span className="font-semibold">Date:</span> {transaction.date}
-                      </div>
-                      <div className="text-xs text-slate-600 dark:text-slate-300 my-1">
-                        <span className="font-semibold">Time:</span> {transaction.time}
-                      </div>
-                      <div className="text-xs text-slate-600 dark:text-slate-300 my-1">
-                        <span className="font-semibold">Type:</span> {transaction.trans_type}
-                      </div>
-                      <div className="flex justify-start items-center text-xs text-slate-600 dark:text-slate-300 my-1">
-                        <span className="font-semibold">Status:</span>&nbsp;<StatusBadgeDesktop statusType={transaction.status_type} />
-                      </div>
-                      <div className="text-xs text-slate-600 dark:text-slate-300 my-1">
-                        <span className="font-semibold">Amount:</span>  {formatAmount(transaction.amount)}
-                      </div>
-                      <div className="mt-5 group">
-                        <div onClick={() => handleOpenModal(transaction)} className="flex justify-center items-center text-center p-1 border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white rounded-full mt-2">
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 mr-0.5">
-                            <path className="stroke-blue-500" strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-                            <path className="stroke-blue-500" strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                          </svg>
-                          <span className="text-xs font-normal">&nbsp;View Details</span>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  ))
-                )}
             </div>
+          </div>
         </div>
         </>
       );
 }
-     
 
-export default TransMobile;
+export default TransDesktop;
