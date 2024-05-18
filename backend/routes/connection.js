d1ac56501c5f2b0e8772146266f82af0ea782629
Frostoverthesheep <atanaciobasa9@gmail.com>
2024-05-19 00:13:13 +0800

    fix db

--- backend/routes/connection.js
+++ backend/routes/connection.js
@@ -1,27 +1,41 @@
 import mysql from "mysql";
 import dotenv from 'dotenv';
 dotenv.config();
+
 const host = process.env.DB_HOST;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 const database = process.env.DB_DATABASE;
 
-const conn2 = mysql.createConnection({
-  host: host,
-  user: user,
-  password: password,
-  database: database,
-});
-  
+let conn2;
+
+function handleDisconnect() {
+  conn2 = mysql.createConnection({
+    host: host,
+    user: user,
+    password: password,
+    database: database,
+  });
+
   conn2.connect((err) => {
     if (err) {
       console.error("Error connecting to the database: " + err);
-      return;
+      setTimeout(handleDisconnect, 1500);  // Retry connection after 2 seconds
+    } else {
+      console.log("Connected to the clientdatabase");
     }
-    console.log("Connected to the clientdatabase");
   });
 
+  conn2.on('error', (err) => {
+    console.error('Database error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();  // Reconnect if the connection is lost
+    } else {
+      throw err;  // Handle other errors
+    }
+  });
+}
+
+handleDisconnect();
 
 export default conn2;
-
-
--- backend/routes/connection1.js
+++ backend/routes/connection1.js
@@ -5,25 +5,37 @@ dotenv.config();
 const host = process.env.DB_HOST1;
 const user = process.env.DB_USER1;
 const password = process.env.DB_PASSWORD1;
-const database1 = process.env.DB_DATABASE1;
+const database = process.env.DB_DATABASE1;
 
+let conn1;
 
-const conn1 = mysql.createConnection({
+function handleDisconnect() {
+  conn1 = mysql.createConnection({
     host: host,
     user: user,
     password: password,
-    database: database1,
+    database: database,
   });
-  
+
   conn1.connect((err) => {
     if (err) {
       console.error("Error connecting to the database: " + err);
-      return;
+      setTimeout(handleDisconnect, 2000);  // Retry connection after 2 seconds
+    } else {
+      console.log("Connected to the admindatabase");
     }
-    console.log("Connected to the admindatabase");
   });
 
+  conn1.on('error', (err) => {
+    console.error('Database error:', err);
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();  // Reconnect if the connection is lost
+    } else {
+      throw err;  // Handle other errors
+    }
+  });
+}
+
+handleDisconnect();
 
 export default conn1;
-
-
