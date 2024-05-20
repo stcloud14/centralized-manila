fea456645ff0e9b6fbce9c32a920b001225f0a9a
Frostoverthesheep <atanaciobasa9@gmail.com>
2024-05-20 18:38:47 +0800

    Removing redundant

--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -465,11 +465,6 @@ console.log(selectedFiles)
 
                 {passwordError && <h3 className="text-red-500 text-xs md:text-sm">{passwordError}</h3>}
 
-                {/* <div class="relative z-0 w-full mb-6 group">
-                    <input type="password" name="user_pass1" id="user_pass1" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer mobnum" placeholder=" " required />
-                    <label for="user_pass1" class="peer-focus:font-medium absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm Password</label>
-                </div> */}
-
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-3 md:gap-6">
                   <div className="relative z-0 w-full group">
                     <h1 className="italic text-xs text-slate-400">Password must be:</h1>
