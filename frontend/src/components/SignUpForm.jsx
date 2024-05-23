e4c4c015990d222e7513632c78a107cec30124f6
Frostoverthesheep <atanaciobasa9@gmail.com>
2024-05-24 01:49:47 +0800

    Fix

--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -24,6 +24,7 @@ const SignUpForm =()=>{
     setUserReg({ user_email: "" });
     setEmailFromParams(false);
     localStorage.removeItem('email');
+    localStorage.removeItem('photoURL')
   };
   
   useEffect(() => {
