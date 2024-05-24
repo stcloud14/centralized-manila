ad4d16013c06b87710fc7252c7cba02cc79c5624
Miaaaaah <jeremiahpaul612@gmail.com>
2024-05-24 21:26:58 +0800

    adminaddform

--- frontend/src/admin_components/AdminAddAdminForm.jsx
+++ frontend/src/admin_components/AdminAddAdminForm.jsx
@@ -17,6 +17,19 @@ import VerifyModal from '../partials/business/VerifyModal';
 const AdminAddAdminForm =()=>{
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
+  const [adminId, setAdminId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [adminType, setAdminType] = useState(0);
+
+  const handleAdminTypeChange = (e) => {
+    setAdminType(e.target.value);
+  };
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [isCompleteConfirm, setIsCompleteConfirm] = useState(false);
+
   return (
     <div className="flex h-screen overflow-hidden dark:bg-[#212121]">
 
@@ -31,6 +44,32 @@ const AdminAddAdminForm =()=>{
 
 
     <main className="overflow-y-auto">
+    {/* {isLoading && (
+              <div className="flex flex-col items-center justify-center h-[500px]">
+              
+                <svg
+                  aria-hidden="true"
+                  className="w-10 h-10 md:w-15 md:h-15 lg:w-20 lg:h-20 pb-0 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <p className="pt-5 sm:pt-10 font-bold text-lg md:text-xl">Please wait for a moment...</p>
+              
+              </div>
+            )}
+          
+          {!isLoading && ( */}
+      
       <div className="flex flex-col col-span-full sm:col-span-6 xl:col-span-4 bg-white dark:bg-[#2b2b2b] dark:border-[#3d3d3d] shadow-lg rounded-sm border border-slate-200 mx-4 mt-4 mb-2">
         <div className="px-5 py-5">
         <h1 className='font-medium text-center text-slate-700 dark:text-white'>Chief Admin</h1>
@@ -42,13 +81,13 @@ const AdminAddAdminForm =()=>{
 
                     <div className="mt-5 relative z-0 mb-2 grid grid-cols-2 gap-6">
                       <div className="relative flex flex-col col-span-1">
-                        <input type="text" name="" id="" placeholder=" " onChange="" value="" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"/>
+                        <input type="text" name="adminid" id="adminid" placeholder=" " onChange={(e) => setAdminId(e.target.value)} value={adminId} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"/>
                         <label htmlFor="" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                           Admin ID
                         </label>
                       </div>
                       <div className="relative flex flex-col group col-span-1">
-                        <select onChange="" value="" defaultValue={0} name="period" id="period" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer cursor-pointer">
+                        <select onChange={handleAdminTypeChange} value={adminType} defaultValue={0} name="admintype" id="admintype" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer cursor-pointer">
                           <option value="0" className='dark:bg-[#3d3d3d]'>Select Admin Type</option>
                           <option value="1" className='dark:bg-[#3d3d3d]'>Chief Admin</option>
                           <option value="2" className='dark:bg-[#3d3d3d]'>RP Tax Admin</option>
@@ -66,7 +105,7 @@ const AdminAddAdminForm =()=>{
                     
                     <div className="relative z-0 mb-2 grid grid-cols-5 gap-6">
                       <div class="flex flex-col col-span-full  sm:col-span-3">
-                        <input type="text" name="" id="" placeholder=" " onChange="" value="" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"/>
+                        <input type="text" name="password" id="password" placeholder=" " onChange={(e) => setPassword(e.target.value)}  value={password} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"/>
                         <label htmlFor="" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                             Password
                           </label>
@@ -84,17 +123,66 @@ const AdminAddAdminForm =()=>{
 
                   {/* Submit Button */}
                   <div className="flex justify-end items-end mt-10 mb-4">
-                    <button 
-                        onClick=""
-                        type="submit" 
-                        className="text-blue-500 hover:text-white border border-blue-500 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-full text-sm px-10 py-2.5 text-center mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
-                          Proceed
+                    <button onClick="" type="submit" className="text-blue-500 hover:text-white border border-blue-500 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-full text-sm px-10 py-2.5 text-center mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
+                         + Add Admin
                     </button>
                   </div>
+                              {/* PROCESS MODAL */}
+          {isCompleteConfirm && (
+              <div className="fixed z-50 inset-0 overflow-y-auto">
+                <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                  <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+                    <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+                  </div>
+                  <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+                    &#8203;
+                  </span>
+                  <div className="inline-block align-bottom bg-white rounded-sm text-center overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+                    <div className="bg-white dark:bg-[#212121] px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                      <div className="mx-auto mt-4">
+                        <span className="font-medium text-slate-700 dark:text-white sm:mt-0 text-xs md:text-sm" id="modal-headline">
+                          Would you like to mark this transaction as complete?
+                        </span>
+                      </div>
+                    </div>
+
+
+                    
+                    {isLoading ? (
+                      <div className="bg-white dark:bg-[#212121] text-slate-700 dark:text-white px-1 pb-8 rounded-b-sm">
+                        <Loading />
+                      </div>
+                    ) : (
+                      <>
+                    <div className="bg-white dark:bg-[#212121] px-4 py-3 gap-3 sm:px-6 flex justify-end">
+                      <button
+                        onClick={handleConfirmClose}
+                        type="button"
+                        className="text-slate-500 text-xs md:text-sm ms-2 hover:text-white border border-slate-500 hover:bg-slate-500 focus:ring-4 focus:outline-none focus:ring-slate-300 font-normal rounded-sm px-5 py-2 text-center mb-2 dark:border-slate-500 dark:text-white dark:hover:text-white dark:hover:bg-slate-500 dark:focus:ring-slate-800"
+                      >
+                        <p>Cancel</p>
+                      </button>
+                    
+                    <button
+                    onClick={handleComplete} 
+                    type="button"
+                    className="text-white text-xs md:text-sm bg-emerald-500 border border-emerald-500 hover:bg-emerald-600 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-normal rounded-sm px-5 py-2 text-center mb-2 dark:border-emerald-500 dark:text-white dark:hover:text-white dark:hover:border-emerald-700 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
+                    >
+                      Confirm
+                    </button>
+                    </div>
+                    </>
+                    )}
+
+                  </div>
+                </div>
+              </div>
+            )}
               </form>
               </form>
             </div>
           </div>
+            {/* )} */}
           <AdminFooter />
         </main>
 
