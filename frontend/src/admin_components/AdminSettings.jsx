522f0bf96bab737669eb8a16ed9c158336a92665
Frostoverthesheep <atanaciobasa9@gmail.com>
2024-05-24 22:08:08 +0800

    token

--- frontend/src/admin_components/AdminSettings.jsx
+++ frontend/src/admin_components/AdminSettings.jsx
@@ -24,6 +24,33 @@ const AdminSettings =()=>{
   const { pathname } = location;
   const user_id = pathname.split("/")[2];
 
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    
+    const checkToken = async (token) => {
+
+            const response = await axios.get(`${Base_Url}token/protect-token-admin/admin/${admin_type}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            const { admin_type } = response.data;
+            if (admin_type) {
+                // Allow access to the audit page
+                setReload(false);
+            } else {
+                window.location.href = '/indexadmin';
+            }
+    };
+
+    if (token) {
+        checkToken(token);
+    } else {
+        // Redirect to indexadmin if token is not present
+        window.location.href = '/indexadmin';
+    }
+}, []);
+
   let imageUrl;
   let userName;
 
