08660a0753f0546c1850bc5309a436285939c57c
Miaaaaah <jeremiahpaul612@gmail.com>
2024-05-26 23:06:52 +0800

    Admin Name

--- frontend/src/admin_components/AdminAddAdminForm.jsx
+++ frontend/src/admin_components/AdminAddAdminForm.jsx
@@ -188,6 +188,12 @@ const AdminAddAdminForm =()=>{
                       Admin ID
                     </label>
                   </div>
+                  <div className="relative z-0 w-full mb-6 group">
+                    <input type="text" name="adminid" id="adminid" placeholder=" " onChange={(e) => setMobileNo(e.target.value)} value={mobileNo} className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"/>
+                    <label htmlFor="" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                      Admin Name
+                    </label>
+                  </div>
                   <div class="relative z-0 w-full mb-4 group col-span-2">
                     <input type="text" name="password" id="password" placeholder=" "  value={password} readOnly  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"/>
                     <label htmlFor="" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
