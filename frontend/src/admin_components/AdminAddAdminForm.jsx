b6ffd97c60e6dad4de3e9dbb40576db5bee135cf
Miaaaaah <jeremiahpaul612@gmail.com>
2024-05-26 17:13:13 +0800

    adminaddform ui fix

--- frontend/src/admin_components/AdminAddAdminForm.jsx
+++ frontend/src/admin_components/AdminAddAdminForm.jsx
@@ -199,7 +199,7 @@ const AdminAddAdminForm =()=>{
                         <button 
                           onClick={generatePassword}
                           type="button" 
-                          className="w-full text-blue-500 hover:text-white border border-blue-500 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-full text-sm px-4 py-2.5 text-center ml-0 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
+                          className="w-full text-blue-500 hover:text-white border border-blue-500 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-sm text-sm px-4 py-2.5 text-center ml-0 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
                           Generate
                         </button>
                       </div>
@@ -222,7 +222,7 @@ const AdminAddAdminForm =()=>{
 
                   {/* Submit Button */}
                   <div className="flex justify-end items-end mt-10 mb-4">
-                    <button onClick={handleSubmit} type="submit" className="text-blue-500 hover:text-white border border-blue-500 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-full text-sm px-10 py-2.5 text-center mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
+                    <button onClick={handleSubmit} type="submit" className="text-blue-500 hover:text-white border border-blue-500 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-normal rounded-sm text-sm px-10 py-2.5 text-center mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
                          + Add Admin
                     </button>
                   </div>
